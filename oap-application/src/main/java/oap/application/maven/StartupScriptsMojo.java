bbf40ed0b02df2be006cd6d2b9f5de13851401ef
Volodymyr Kyrychenko <vladimir.kirichenko@gmail.com>
2015-10-29 06:02:05 +0200

    fix permissions

--- oap-application/src/main/java/oap/application/maven/StartupScriptsMojo.java
+++ oap-application/src/main/java/oap/application/maven/StartupScriptsMojo.java
@@ -55,7 +55,7 @@ public class StartupScriptsMojo extends AbstractMojo {
         Path functions = Files.path( destinationDirectory, serviceBin, "functions.sh" );
         Resources.readString( getClass(), "/bin/functions.sh" )
             .ifPresent( value -> Files.writeString( functions, value ) );
-        PosixFilePermission[] permissions = { OWNER_EXECUTE, OWNER_READ, GROUP_READ, OTHERS_READ };
+        PosixFilePermission[] permissions = { OWNER_EXECUTE, OWNER_READ, OWNER_WRITE, GROUP_READ, OTHERS_READ };
         Files.chmod( functions, permissions );
         script( "/bin/oap.sh", serviceBin, ".sh", permissions );
         script( "/bin/service.systemd", "usr/lib/systemd/system", ".service" );
