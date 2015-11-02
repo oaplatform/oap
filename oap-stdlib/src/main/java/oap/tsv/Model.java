af5812e20ce9f0c74c3f11a9d0f1b381ce989073
Volodymyr Kyrychenko <vladimir.kirichenko@gmail.com>
2015-11-02 19:14:08 +0200

    join with surrounding

--- oap-stdlib/src/main/java/oap/tsv/Model.java
+++ oap-stdlib/src/main/java/oap/tsv/Model.java
@@ -30,7 +30,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import static oap.util.Lists.Collectors.toArrayList;
 
