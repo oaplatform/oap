b488b72a6d02d50a17af2d1de2d61993b1387989
Volodymyr Kyrychenko <vladimir.kirichenko@gmail.com>
2015-12-30 17:10:55 +0200

    copy mojo

---
+++ oap-maven/src/main/java/net/madberry/maven/CopyMojo.java
@@ -0,0 +1,57 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) Open Application Platform Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package net.madberry.maven;
+
+import oap.io.Files;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.util.List;
+import java.util.Properties;
+
+@Mojo( name = "copy", defaultPhase = LifecyclePhase.PREPARE_PACKAGE )
+public class CopyMojo extends AbstractMojo {
+    @Parameter( required = true )
+    private String targetDirectory;
+
+    @Parameter( defaultValue = "${project}", readonly = true, required = true )
+    private MavenProject project;
+
+    @Parameter( required = true )
+    private List<FileSet> fileSets;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        Properties properties = project.getProperties();
+        for( FileSet file : fileSets )
+            Files.copyContent( Files.path( file.getDirectory() ), Files.path( targetDirectory ),
+                file.getIncludes(), file.getExcludes(),
+                file.isFiltering(), properties::get );
+    }
+}
--- oap-stdlib/src/main/java/oap/io/Files.java
+++ oap-stdlib/src/main/java/oap/io/Files.java
@@ -23,11 +23,13 @@
  */
 package oap.io;
 
+import oap.util.Lists;
 import oap.util.Sets;
 import oap.util.Stream;
 import oap.util.Strings;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.codehaus.plexus.util.DirectoryScanner;
 
 import java.io.File;
 import java.io.IOException;
@@ -36,6 +38,7 @@ import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.nio.file.SimpleFileVisitor;
@@ -43,9 +46,11 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.PosixFilePermission;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public final class Files {
     public static ArrayList<Path> wildcard( String basePath, String wildcard ) {
@@ -169,17 +174,54 @@ public final class Files {
         }
     }
 
-    public static void chmod( Path path, PosixFilePermission... permissions ) {
+    public static void copyContent( Path basePath, Path destPath ) {
+        copyContent( basePath, destPath, Lists.of( "**/*" ), Lists.of() );
+    }
+
+    public static void copyContent( Path basePath, Path destPath, List<String> includes, List<String> excludes ) {
+        copyContent( basePath, destPath, includes, excludes, false, null );
+    }
+
+    public static void copyContent( Path basePath, Path destPath, List<String> includes, List<String> excludes,
+        boolean filtering, Function<String, Object> mapper ) {
+        DirectoryScanner scanner = new DirectoryScanner();
+        scanner.setBasedir( basePath.toFile() );
+        scanner.setIncludes( includes.toArray( new String[includes.size()] ) );
+        scanner.setExcludes( excludes.toArray( new String[excludes.size()] ) );
+        scanner.scan();
+        for( String included : scanner.getIncludedFiles() ) {
+            Path src = basePath.resolve( included );
+            Path dst = destPath.resolve( included );
+            if( filtering ) Files.writeString( dst, Strings.substitute( Files.readString( src ), mapper ) );
+            else copy( src, IoStreams.Encoding.PLAIN, dst, IoStreams.Encoding.PLAIN );
+            setPosixPermissions( dst, getPosixPermissions( src ) );
+        }
+    }
+
+    public static void setPosixPermissions( Path path, Set<PosixFilePermission> permissions ) {
         try {
-            java.nio.file.Files.setPosixFilePermissions( path, Sets.of( permissions ) );
+            java.nio.file.Files.setPosixFilePermissions( path, permissions );
         } catch( IOException e ) {
             throw new UncheckedIOException( e );
         }
+
     }
 
     public static void rename( Path sourcePath, Path destPath ) {
         if( destPath.toFile().exists() ) delete( destPath );
         if( !sourcePath.toFile().renameTo( destPath.toFile() ) )
             throw new UncheckedIOException( new IOException( "cannot rename " + sourcePath + " to " + destPath ) );
+    }
+
+    public static void setPosixPermissions( Path path, PosixFilePermission... permissions ) {
+        setPosixPermissions( path, Sets.of( permissions ) );
+    }
+
+    public static Set<PosixFilePermission> getPosixPermissions( Path path ) {
+        try {
+            return java.nio.file.Files.getPosixFilePermissions( path, LinkOption.NOFOLLOW_LINKS );
+        } catch( IOException e ) {
+            throw new UncheckedIOException( e );
+        }
     }
 }
