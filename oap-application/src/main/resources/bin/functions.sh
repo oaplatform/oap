d93a3579eac4ace888d6f9f6b4ae4a1ba71c13ab
Igor Petrenko <igor.petrenko@madberry.net>
2016-02-08 14:32:11 +0200

    Boot::add config-directory

--- oap-application/src/main/resources/bin/functions.sh
+++ oap-application/src/main/resources/bin/functions.sh
@@ -45,7 +45,7 @@ oap_status_q() {
 
 
 oap_start() {
-    $SUEXEC java $VM_OPTS -cp $CP oap.application.Boot --start --config=$APPHOME/conf/application.conf > /var/log/$APPNAME/console.log 2>&1 &
+    $SUEXEC java $VM_OPTS -cp $CP oap.application.Boot --start $CONFIGDIRECTORY --config=$APPHOME/conf/application.conf > /var/log/$APPNAME/console.log 2>&1 &
     retval=$?
     pid=$!
     [ $retval -eq 0 ] && echo $pid > $PIDFILE
--- oap-application/src/main/resources/bin/oap.sh
+++ oap-application/src/main/resources/bin/oap.sh
@@ -6,6 +6,7 @@ APPHOME=`pwd`
 APPNAME=`basename $APPHOME`
 
 . $APPHOME/bin/functions.sh
+[ -f $APPHOME/bin/env.sh ] && . $APPHOME/bin/env.sh
 
 case $1 in
     "--start")
