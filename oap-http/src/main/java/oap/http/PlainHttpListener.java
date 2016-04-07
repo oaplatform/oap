9e02ef2ae706df3e1c133c78f1854f919cb08112
Iurii Ignatko <yura.ignatko@gmail.com>
2016-04-07 17:04:06 +0300

    Log already used port on socket bind exception

--- oap-http/src/main/java/oap/http/PlainHttpListener.java
+++ oap-http/src/main/java/oap/http/PlainHttpListener.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.net.BindException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 
@@ -26,6 +27,9 @@ public class PlainHttpListener extends AbstractHttpListener {
          serverSocket.bind( new InetSocketAddress( port ) );
 
          return serverSocket;
+      } catch( BindException e ) {
+         log.error( "Cannot bind to port [{}]", port );
+         throw new UncheckedIOException( e );
       } catch( IOException e ) {
          throw new UncheckedIOException( e );
       }
