96d13a4f015ce0af5ebe515dbba8a316378e2e78
Igor Petrenko <igor.petrenko@madberry.net>
2016-04-11 11:57:20 +0300

    quartz: logging

--- oap-stdlib/src/main/java/oap/concurrent/scheduler/Scheduler.java
+++ oap-stdlib/src/main/java/oap/concurrent/scheduler/Scheduler.java
@@ -35,67 +35,70 @@ import static org.quartz.JobBuilder.newJob;
 import static org.quartz.TriggerBuilder.newTrigger;
 
 public class Scheduler {
-    static final org.quartz.Scheduler scheduler;
-    static final FunctionJobFactory jobFactory;
-    private static final AtomicLong ids = new AtomicLong();
+   static final org.quartz.Scheduler scheduler;
+   static final FunctionJobFactory jobFactory;
+   private static final AtomicLong ids = new AtomicLong();
 
-    static {
-        try {
-            final StdSchedulerFactory sf = new StdSchedulerFactory();
-            Properties props = new Properties();
-            props.setProperty( StdSchedulerFactory.PROP_SCHED_SKIP_UPDATE_CHECK, "true" );
-            props.setProperty( StdSchedulerFactory.PROP_JOB_STORE_CLASS, "org.quartz.simpl.RAMJobStore" );
-            props.setProperty( "org.quartz.threadPool.threadCount",
-                String.valueOf( Runtime.getRuntime().availableProcessors() ) );
-            sf.initialize( props );
+   static {
+      try {
+         final StdSchedulerFactory sf = new StdSchedulerFactory();
+         Properties props = new Properties();
+         props.setProperty( StdSchedulerFactory.PROP_SCHED_SKIP_UPDATE_CHECK, "true" );
+         props.setProperty( StdSchedulerFactory.PROP_JOB_STORE_CLASS, "org.quartz.simpl.RAMJobStore" );
+         props.setProperty( "org.quartz.threadPool.threadCount",
+            String.valueOf( Runtime.getRuntime().availableProcessors() ) );
+         sf.initialize( props );
 
-            scheduler = sf.getScheduler();
-            scheduler.setJobFactory( jobFactory = new FunctionJobFactory() );
-            scheduler.start();
-        } catch( org.quartz.SchedulerException e ) {
-            throw new SchedulerException( e );
-        }
-    }
+         scheduler = sf.getScheduler();
+         scheduler.setJobFactory( jobFactory = new FunctionJobFactory() );
 
-    private Scheduler() {
-    }
+         scheduler.getListenerManager().addJobListener( new SchedulerLogging() );
 
-    public static Scheduled scheduleWithFixedDelay( long delay, TimeUnit unit, Runnable runnable ) {
-        return schedule( runnable,
-            SimpleScheduleBuilder.simpleSchedule()
-                .withIntervalInMilliseconds( unit.toMillis( delay ) )
-                .withMisfireHandlingInstructionIgnoreMisfires()
-                .repeatForever() );
-    }
+         scheduler.start();
+      } catch( org.quartz.SchedulerException e ) {
+         throw new SchedulerException( e );
+      }
+   }
 
-    private static Scheduled schedule( Runnable runnable, ScheduleBuilder<?> scheduleBuilder ) {
-        try {
-            String identity = identity( runnable );
+   private Scheduler() {
+   }
 
-            JobDetail job = newJob( RunnableJob.class )
-                .withIdentity( identity + "/job" )
-                .storeDurably()
-                .build();
+   public static Scheduled scheduleWithFixedDelay( long delay, TimeUnit unit, Runnable runnable ) {
+      return schedule( runnable,
+         SimpleScheduleBuilder.simpleSchedule()
+            .withIntervalInMilliseconds( unit.toMillis( delay ) )
+            .withMisfireHandlingInstructionIgnoreMisfires()
+            .repeatForever() );
+   }
 
-            Trigger trigger = newTrigger()
-                .withIdentity( identity + "/trigger" )
-                .withSchedule( scheduleBuilder )
-                .build();
+   private static Scheduled schedule( Runnable runnable, ScheduleBuilder<?> scheduleBuilder ) {
+      try {
+         String identity = identity( runnable );
 
-            jobFactory.register( job, runnable );
+         JobDetail job = newJob( RunnableJob.class )
+            .withIdentity( identity + "/job" )
+            .storeDurably()
+            .build();
 
-            scheduler.scheduleJob( job, trigger );
-            return new Scheduled( job );
-        } catch( org.quartz.SchedulerException e ) {
-            throw new SchedulerException( e );
-        }
-    }
+         Trigger trigger = newTrigger()
+            .withIdentity( identity + "/trigger" )
+            .withSchedule( scheduleBuilder )
+            .build();
 
-    public static Scheduled scheduleCron( String cron, Runnable runnable ) {
-        return schedule( runnable, CronScheduleBuilder.cronSchedule( cron ) );
-    }
+         jobFactory.register( job, runnable );
 
-    private static String identity( Runnable runnable ) {
-        return runnable.getClass().getName() + "/" + ids.incrementAndGet();
-    }
+         scheduler.scheduleJob( job, trigger );
+         return new Scheduled( job );
+      } catch( org.quartz.SchedulerException e ) {
+         throw new SchedulerException( e );
+      }
+   }
+
+   public static Scheduled scheduleCron( String cron, Runnable runnable ) {
+      return schedule( runnable, CronScheduleBuilder.cronSchedule( cron ) );
+   }
+
+   private static String identity( Runnable runnable ) {
+      return runnable.getClass().getName() + "/" + ids.incrementAndGet();
+   }
 }
