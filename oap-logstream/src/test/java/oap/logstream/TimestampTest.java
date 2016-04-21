c283641e44de18c694d4c4c45325ba0548b5f240
macchiatow <cmeisters@gmail.com>
2016-04-21 16:06:16 +0300

    Timestamp.timestampsBeforeNow

--- oap-logstream/src/test/java/oap/logstream/TimestampTest.java
+++ oap-logstream/src/test/java/oap/logstream/TimestampTest.java
@@ -26,6 +26,7 @@ package oap.logstream;
 
 import oap.testng.AbstractTest;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeUtils;
 import org.testng.annotations.Test;
 
 import java.util.Optional;
@@ -35,89 +36,107 @@ import static org.joda.time.DateTimeZone.UTC;
 
 public class TimestampTest extends AbstractTest {
 
-    @Test
-    public void format() throws Exception {
-        assertThat( Timestamp.format( new DateTime( 2015, 12, 3, 11, 28, 30, UTC ), 12 ) ).isEqualTo( "2015-12-03-11-05" );
-    }
+   @Test
+   public void format() throws Exception {
+      assertThat( Timestamp.format( new DateTime( 2015, 12, 3, 11, 28, 30, UTC ), 12 ) ).isEqualTo( "2015-12-03-11-05" );
+   }
 
-    @Test
-    public void directoryName() throws Exception {
-        assertThat( Timestamp.directoryName( "2015-12-03-11-05" ) ).isEqualTo( "2015-12/03" );
-    }
+   @Test
+   public void directoryName() throws Exception {
+      assertThat( Timestamp.directoryName( "2015-12-03-11-05" ) ).isEqualTo( "2015-12/03" );
+   }
 
-    @Test
-    public void path() {
-        DateTime date = new DateTime( 2015, 12, 3, 11, 28, 30, UTC );
-        String timestamp = Timestamp.format( date, 12 );
-        assertThat( Timestamp.path( "log", date, "dir/dir/file", "log.gz", 12 ) )
-            .isEqualTo( "log/dir/dir/2015-12/03/file-2015-12-03-11-05.log.gz" );
-        assertThat( Timestamp.path( "log", timestamp, "dir/dir/file", "log.gz" ) )
-            .isEqualTo( "log/dir/dir/2015-12/03/file-2015-12-03-11-05.log.gz" );
-        assertThat( Timestamp.path( "log", timestamp, "file", "log.gz" ) )
-            .isEqualTo( "log/2015-12/03/file-2015-12-03-11-05.log.gz" );
-        assertThat( Timestamp.path( "*", timestamp, "dir/dir/file", "log.gz" ) )
-            .isEqualTo( "*/dir/dir/2015-12/03/file-2015-12-03-11-05.log.gz" );
-    }
+   @Test
+   public void path() {
+      DateTime date = new DateTime( 2015, 12, 3, 11, 28, 30, UTC );
+      String timestamp = Timestamp.format( date, 12 );
+      assertThat( Timestamp.path( "log", date, "dir/dir/file", "log.gz", 12 ) )
+         .isEqualTo( "log/dir/dir/2015-12/03/file-2015-12-03-11-05.log.gz" );
+      assertThat( Timestamp.path( "log", timestamp, "dir/dir/file", "log.gz" ) )
+         .isEqualTo( "log/dir/dir/2015-12/03/file-2015-12-03-11-05.log.gz" );
+      assertThat( Timestamp.path( "log", timestamp, "file", "log.gz" ) )
+         .isEqualTo( "log/2015-12/03/file-2015-12-03-11-05.log.gz" );
+      assertThat( Timestamp.path( "*", timestamp, "dir/dir/file", "log.gz" ) )
+         .isEqualTo( "*/dir/dir/2015-12/03/file-2015-12-03-11-05.log.gz" );
+   }
 
-    @Test
-    public void timestamps() {
-        assertThat( Timestamp.timestampsBefore( new DateTime( 2016, 2, 1, 1, 1, 1, UTC ), 10, 12 ) )
-            .containsExactly(
-                "2016-02-01-00-03",
-                "2016-02-01-00-04",
-                "2016-02-01-00-05",
-                "2016-02-01-00-06",
-                "2016-02-01-00-07",
-                "2016-02-01-00-08",
-                "2016-02-01-00-09",
-                "2016-02-01-00-10",
-                "2016-02-01-00-11",
-                "2016-02-01-01-00"
-            );
-        assertThat( Timestamp.timestampsBefore( new DateTime( 2016, 2, 1, 1, 1, 1, UTC ), 10, 12 ) )
-            .containsExactly(
-                "2016-02-01-00-03",
-                "2016-02-01-00-04",
-                "2016-02-01-00-05",
-                "2016-02-01-00-06",
-                "2016-02-01-00-07",
-                "2016-02-01-00-08",
-                "2016-02-01-00-09",
-                "2016-02-01-00-10",
-                "2016-02-01-00-11",
-                "2016-02-01-01-00"
-            );
-        assertThat( Timestamp.timestampsAfter( new DateTime( 2016, 2, 1, 1, 1, 1, UTC ), 10, 12 ) )
-            .containsExactly(
-                "2016-02-01-01-00",
-                "2016-02-01-01-01",
-                "2016-02-01-01-02",
-                "2016-02-01-01-03",
-                "2016-02-01-01-04",
-                "2016-02-01-01-05",
-                "2016-02-01-01-06",
-                "2016-02-01-01-07",
-                "2016-02-01-01-08",
-                "2016-02-01-01-09"
-            );
-    }
+   @Test
+   public void timestamps() {
+      assertThat( Timestamp.timestampsBefore( new DateTime( 2016, 2, 1, 1, 1, 1, UTC ), 10, 12 ) )
+         .containsExactly(
+            "2016-02-01-00-03",
+            "2016-02-01-00-04",
+            "2016-02-01-00-05",
+            "2016-02-01-00-06",
+            "2016-02-01-00-07",
+            "2016-02-01-00-08",
+            "2016-02-01-00-09",
+            "2016-02-01-00-10",
+            "2016-02-01-00-11",
+            "2016-02-01-01-00"
+         );
+      assertThat( Timestamp.timestampsBefore( new DateTime( 2016, 2, 1, 1, 1, 1, UTC ), 10, 12 ) )
+         .containsExactly(
+            "2016-02-01-00-03",
+            "2016-02-01-00-04",
+            "2016-02-01-00-05",
+            "2016-02-01-00-06",
+            "2016-02-01-00-07",
+            "2016-02-01-00-08",
+            "2016-02-01-00-09",
+            "2016-02-01-00-10",
+            "2016-02-01-00-11",
+            "2016-02-01-01-00"
+         );
+      assertThat( Timestamp.timestampsAfter( new DateTime( 2016, 2, 1, 1, 1, 1, UTC ), 10, 12 ) )
+         .containsExactly(
+            "2016-02-01-01-00",
+            "2016-02-01-01-01",
+            "2016-02-01-01-02",
+            "2016-02-01-01-03",
+            "2016-02-01-01-04",
+            "2016-02-01-01-05",
+            "2016-02-01-01-06",
+            "2016-02-01-01-07",
+            "2016-02-01-01-08",
+            "2016-02-01-01-09"
+         );
 
-    @Test
-    public void parse() {
-        DateTime[] times = {
-            new DateTime( 2016, 2, 1, 1, 0, UTC ),
-            new DateTime( 2016, 2, 1, 1, 55, UTC ),
-            new DateTime( 2016, 2, 1, 1, 5, UTC ),
-            new DateTime( 2016, 2, 1, 1, 15, UTC )
-        };
-        for( DateTime time : times )
-            assertThat( Timestamp.parse( Timestamp.format( time, 12 ), 12 ) ).isEqualTo( time );
-    }
+      DateTimeUtils.setCurrentMillisFixed( new DateTime( 2016, 4, 21, 1, 0 ).getMillis() );
 
-    @Test
-    public void testParseFileNameWithTimestamp() {
-        final Optional<DateTime> dateTime = Timestamp.parseFileNameWithTimestamp( "/tmp/test/2016-02/01/tes-t1-2016-02-01-01-00.tsv.gz", 12 );
+      assertThat( Timestamp.timestampsBeforeNow( new DateTime( 2016, 4, 21, 0, 1 ), 12 ) )
+         .containsExactly(
+            "2016-04-20-21-00",
+            "2016-04-20-21-01",
+            "2016-04-20-21-02",
+            "2016-04-20-21-03",
+            "2016-04-20-21-04",
+            "2016-04-20-21-05",
+            "2016-04-20-21-06",
+            "2016-04-20-21-07",
+            "2016-04-20-21-08",
+            "2016-04-20-21-09",
+            "2016-04-20-21-10",
+            "2016-04-20-21-11"
+         );
+   }
 
-        assertThat( dateTime ).contains( new DateTime( 2016, 2, 1, 1, 0, UTC ) );
-    }
+   @Test
+   public void parse() {
+      DateTime[] times = {
+         new DateTime( 2016, 2, 1, 1, 0, UTC ),
+         new DateTime( 2016, 2, 1, 1, 55, UTC ),
+         new DateTime( 2016, 2, 1, 1, 5, UTC ),
+         new DateTime( 2016, 2, 1, 1, 15, UTC )
+      };
+      for( DateTime time : times )
+         assertThat( Timestamp.parse( Timestamp.format( time, 12 ), 12 ) ).isEqualTo( time );
+   }
+
+   @Test
+   public void testParseFileNameWithTimestamp() {
+      final Optional<DateTime> dateTime = Timestamp.parseFileNameWithTimestamp( "/tmp/test/2016-02/01/tes-t1-2016-02-01-01-00.tsv.gz", 12 );
+
+      assertThat( dateTime ).contains( new DateTime( 2016, 2, 1, 1, 0, UTC ) );
+   }
 }
