4b366c77f71e2c811b06b38ea2f24ddde4bc0e9a
Volodymyr Kyrychenko <vladimir.kirichenko@gmail.com>
2016-08-31 16:54:54 +0300

    Timed in ChunkedStorage

--- oap-storage/src/main/java/oap/storage/ChunkedStorage.java
+++ oap-storage/src/main/java/oap/storage/ChunkedStorage.java
@@ -2,11 +2,9 @@ package oap.storage;
 
 import com.google.common.hash.Hashing;
 import lombok.extern.slf4j.Slf4j;
-import oap.concurrent.scheduler.Scheduled;
-import oap.concurrent.scheduler.Scheduler;
+import oap.concurrent.Timed;
 import oap.json.Binder;
 import oap.util.Stream;
-import org.joda.time.DateTimeUtils;
 
 import java.io.Closeable;
 import java.nio.file.Path;
@@ -15,10 +13,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
-
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 /**
  * Created by macchiatow on 8/30/16.
@@ -27,32 +22,29 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 public class ChunkedStorage<T> implements Closeable {
    private final Map<Integer, Chunk> data = new ConcurrentHashMap<>();
    private final int chunks;
-   private final long fsync;
+   private final Timed fsync;
 
-   private final AtomicLong lastFSync = new AtomicLong( 0 );
    private final Function<String, Integer> hasher;
    private final Function<Integer, Path> chunkPath;
-   private final Function<T, String> identity;
+   private final Function<T, String> identify;
 
-   private Scheduled fsyncScheduled;
-
-   public ChunkedStorage( Function<T, String> identity, Path dataLocation, int chunks, long fsync ) {
+   public ChunkedStorage( Function<T, String> identify, Path dataLocation, int chunks, long fsync ) {
       this.hasher = id -> Math.abs( Hashing.murmur3_32().hashBytes( id.getBytes() ).asInt() ) % chunks;
       this.chunkPath = i -> dataLocation.resolve( "chunk" + i + ".gz" );
       this.chunks = chunks;
-      this.fsync = fsync;
-      this.identity = identity;
+      this.identify = identify;
+      this.fsync = Timed.create( fsync, this::fsync );
    }
 
    public void put( T object ) {
-      String id = identity.apply( object );
+      String id = identify.apply( object );
       Objects.requireNonNull( id, "[id] must not be null" );
       Integer chunkId = hasher.apply( id );
       this.put( object, chunkId );
    }
 
    public void put( T object, Integer chunkId ) {
-      String id = identity.apply( object );
+      String id = identify.apply( object );
       data.computeIfAbsent( chunkId, s -> new Chunk() ).records.put( id, object );
    }
 
@@ -78,28 +70,24 @@ public class ChunkedStorage<T> implements Closeable {
             data.put( c, Binder.json.unmarshal( Chunk.class, path ) );
          }
       }
-      this.fsyncScheduled = Scheduler.scheduleWithFixedDelay( fsync, MILLISECONDS, this::fsync );
+      this.fsync.start();
    }
 
    @Override
    public synchronized void close() {
-      Scheduled.cancel( fsyncScheduled );
-      fsync();
+      this.fsync.close();
+      fsync( this.fsync.lastExecuted() );
       data.clear();
    }
 
-   private synchronized void fsync() {
-      long current = DateTimeUtils.currentTimeMillis();
-      long last = lastFSync.get();
-      log.trace( "fsync current: {}, last: {}, data size: {}", current, last, stream().count() );
+   private synchronized void fsync( long last ) {
+      log.trace( "fsync last: {}, data size: {}", last, stream().count() );
 
       data.entrySet().forEach( chunk -> {
          log.trace( "storing chunk {} with size {} profiles", chunk.getKey(), chunk.getValue().records.size() );
          Binder.json.marshal( chunkPath.apply( chunk.getKey() ), chunk.getValue() );
          log.trace( "storing chunk {} done", chunk.getKey() );
       } );
-
-      lastFSync.set( current );
    }
 
    private class Chunk {
