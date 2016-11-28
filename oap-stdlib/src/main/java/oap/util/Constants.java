3afa24cfd4f7ff4ffbe77d3e028f81ac4e8efb01
macchiatow <cmeisters@gmail.com>
2016-11-28 12:28:16 +0200

    add to Constants

--- oap-stdlib/src/main/java/oap/util/Constants.java
+++ oap-stdlib/src/main/java/oap/util/Constants.java
@@ -8,6 +8,7 @@ public class Constants {
    public static final long ONE_SECOND = 1000;
    public static final long ONE_MINUTE = 60 * ONE_SECOND;
    public static final long ONE_HOUR = 60 * ONE_MINUTE;
+   public static final long ONE_DAY = 24 * ONE_HOUR;
    public static final long ONE_MONTH = 2629746000L;
    public static final String NEW_LINE_CHAR = "\n";
 
