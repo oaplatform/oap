81bc35a68c8fe4d85fc2f730878d25e3524548b4
igor.petrenko <nofate@ukr.net>
2016-12-06 14:55:30 +0200

    fix: tests

--- oap-stdlib/src/test/java/oap/util/ThrowablesTest.java
+++ oap-stdlib/src/test/java/oap/util/ThrowablesTest.java
@@ -33,17 +33,17 @@ import java.io.UncheckedIOException;
  * Created by igor.petrenko on 06.12.2016.
  */
 public class ThrowablesTest {
-   @Test( expectedExceptions = UncheckedIOException.class, expectedExceptionsMessageRegExp = "^test$" )
+   @Test( expectedExceptions = UncheckedIOException.class, expectedExceptionsMessageRegExp = "java.io.IOException: test" )
    public void testPropagateIOException() throws Exception {
       throw Throwables.propagate( new IOException( "test" ) );
    }
 
-   @Test( expectedExceptions = UncheckedIOException.class, expectedExceptionsMessageRegExp = "^test$" )
+   @Test( expectedExceptions = RuntimeException.class, expectedExceptionsMessageRegExp = "test" )
    public void testPropagateUncheckedIOException() throws Exception {
-      throw Throwables.propagate( new UncheckedIOException( new IOException( "test" ) ) );
+      throw Throwables.propagate( new RuntimeException( "test") );
    }
 
-   @Test( expectedExceptions = RuntimeException.class, expectedExceptionsMessageRegExp = "^test$" )
+   @Test( expectedExceptions = RuntimeException.class, expectedExceptionsMessageRegExp = "java.lang.Exception: test" )
    public void testPropagateAnyException() throws Exception {
       throw Throwables.propagate( new Exception( "test" ) );
    }
