53496e14687e0142541847b4a26d8d2ab6fe42ea
igor.petrenko <nofate@ukr.net>
2017-09-12 15:10:21 +0300

    fix: Class oap.statsdb.Node does not implement Serializable or externalizable

--- oap-stats-db/src/main/java/oap/statsdb/Node.java
+++ oap-stats-db/src/main/java/oap/statsdb/Node.java
@@ -44,7 +44,9 @@ import java.util.function.Supplier;
 @EqualsAndHashCode
 @ToString
 @Slf4j
-public class Node {
+public class Node implements Serializable {
+    private static final long serialVersionUID = 4194048067764234L;
+
     volatile ConcurrentHashMap<String, Node> db = new ConcurrentHashMap<>();
     @JsonTypeIdResolver( TypeIdFactory.class )
     @JsonTypeInfo( use = JsonTypeInfo.Id.CUSTOM, include = JsonTypeInfo.As.PROPERTY, property = "object:type" )
