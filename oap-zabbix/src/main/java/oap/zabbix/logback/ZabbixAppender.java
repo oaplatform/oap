8bfadbd3ee35ca1e12e5e171b1cb1b39e4a0946e
igor.petrenko <nofate@ukr.net>
2017-10-02 08:49:16 +0300

    add: logback: zabbix appender

--- oap-zabbix/src/main/java/oap/zabbix/logback/ZabbixAppender.java
+++ oap-zabbix/src/main/java/oap/zabbix/logback/ZabbixAppender.java
@@ -25,20 +25,248 @@
 package oap.zabbix.logback;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.net.AbstractSocketAppender;
-import ch.qos.logback.core.spi.PreSerializationTransformer;
+import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.core.net.DefaultSocketConnector;
+import ch.qos.logback.core.net.ObjectWriter;
+import ch.qos.logback.core.net.SocketConnector;
+import ch.qos.logback.core.util.CloseUtil;
+import ch.qos.logback.core.util.Duration;
+import lombok.val;
+import oap.net.Inet;
+import oap.zabbix.Data;
+import oap.zabbix.Request;
+import org.apache.commons.lang3.NotImplementedException;
+
+import javax.net.SocketFactory;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.Collections.singletonList;
 
 /**
  * Created by igor.petrenko on 29.09.2017.
  */
-public class ZabbixAppender extends AbstractSocketAppender<ILoggingEvent> {
-    @Override
-    protected void postProcessEvent( ILoggingEvent event ) {
+public class ZabbixAppender extends AppenderBase<ILoggingEvent> implements SocketConnector.ExceptionHandler {
+    public static final int DEFAULT_QUEUE_SIZE = 128;
+    private static final int prefixLength = "zabbix.".length();
+    private static final int DEFAULT_EVENT_DELAY_TIMEOUT = 100;
+    private static final int DEFAULT_ACCEPT_CONNECTION_DELAY = 5000;
 
+    private int queueSize = DEFAULT_QUEUE_SIZE;
+    private String remoteHost;
+    private int port;
+    private InetAddress address;
+    private Duration eventDelayLimit = new Duration( DEFAULT_EVENT_DELAY_TIMEOUT );
+    private int acceptConnectionTimeout = DEFAULT_ACCEPT_CONNECTION_DELAY;
+    private Future<?> task;
+    private String peerId;
+
+    private LinkedBlockingDeque<ILoggingEvent> deque;
+    private volatile Socket socket;
+    private SocketConnector connector;
+
+    @Override
+    protected void append( ILoggingEvent event ) {
+        if( event == null || !isStarted() )
+            return;
+
+        try {
+            final boolean inserted = deque.offer( event, eventDelayLimit.getMilliseconds(), TimeUnit.MILLISECONDS );
+            if( !inserted ) {
+                addInfo( "Dropping event due to timeout limit of [" + eventDelayLimit + "] being exceeded" );
+            }
+        } catch( InterruptedException e ) {
+            addError( "Interrupted while appending event to SocketAppender", e );
+        }
     }
 
     @Override
-    protected PreSerializationTransformer<ILoggingEvent> getPST() {
-        return new ZabbixPreSerializationTransformer();
+    public void start() {
+        if( isStarted() )
+            return;
+
+        int errorCount = 0;
+        if( port <= 0 ) {
+            errorCount++;
+            addError( "No port was configured for appender" + name + " For more information, please visit http://logback.qos.ch/codes.html#socket_no_port" );
+        }
+
+        if( remoteHost == null ) {
+            errorCount++;
+            addError( "No remote host was configured for appender" + name
+                + " For more information, please visit http://logback.qos.ch/codes.html#socket_no_host" );
+        }
+
+        if( queueSize == 0 ) {
+            addWarn( "Queue size of zero is deprecated, use a size of one to indicate synchronous processing" );
+        }
+
+        if( queueSize < 0 ) {
+            errorCount++;
+            addError( "Queue size must be greater than zero" );
+        }
+
+        if( errorCount == 0 ) {
+            try {
+                address = InetAddress.getByName( remoteHost );
+            } catch( UnknownHostException ex ) {
+                addError( "unknown host: " + remoteHost );
+                errorCount++;
+            }
+        }
+
+
+        if( errorCount == 0 ) {
+            deque = new LinkedBlockingDeque<>( queueSize );
+            peerId = "remote peer " + remoteHost + ":" + port + ": ";
+            connector = createConnector( address, port );
+            task = getContext().getScheduledExecutorService().submit( this::connectSocketAndDispatchEvents );
+            super.start();
+        }
+    }
+
+    @Override
+    public void stop() {
+        if( !isStarted() )
+            return;
+        CloseUtil.closeQuietly( socket );
+        task.cancel( true );
+        super.stop();
+    }
+
+    private void connectSocketAndDispatchEvents() {
+        try {
+            while( socketConnectionCouldBeEstablished() ) {
+                try {
+                    ObjectWriter objectWriter = createObjectWriterForSocket();
+                    addInfo( peerId + "connection established" );
+                    dispatchEvents( objectWriter );
+                } catch( IOException ex ) {
+                    addInfo( peerId + "connection failed: " + ex );
+                } finally {
+                    CloseUtil.closeQuietly( socket );
+                    socket = null;
+                    addInfo( peerId + "connection closed" );
+                }
+            }
+        } catch( InterruptedException ex ) {
+            assert true; // ok... we'll exit now
+        }
+        addInfo( "shutting down" );
+    }
+
+    private void dispatchEvents( ObjectWriter objectWriter ) throws InterruptedException, IOException {
+        while( true ) {
+            ILoggingEvent event = deque.takeFirst();
+            try {
+                val data = new Data( Inet.hostname(), event.getLoggerName().substring( prefixLength ), event.getMessage() );
+                val request = new Request( singletonList( data ) );
+                val zabbixRequest = new ZabbixRequest( request );
+                objectWriter.write( zabbixRequest );
+            } catch( IOException e ) {
+                tryReAddingEventToFrontOfQueue( event );
+                throw e;
+            }
+        }
+    }
+
+    private void tryReAddingEventToFrontOfQueue( ILoggingEvent event ) {
+        final boolean wasInserted = deque.offerFirst( event );
+        if( !wasInserted ) {
+            addInfo( "Dropping event due to socket connection error and maxed out deque capacity" );
+        }
+    }
+
+    private boolean socketConnectionCouldBeEstablished() throws InterruptedException {
+        return ( socket = connector.call() ) != null;
+    }
+
+    private SocketConnector createConnector( InetAddress address, int port ) {
+        SocketConnector connector = newConnector( address, port );
+        connector.setExceptionHandler( this );
+        connector.setSocketFactory( getSocketFactory() );
+        return connector;
+    }
+
+    @Override
+    public void connectionFailed( SocketConnector connector, Exception ex ) {
+
+    }
+
+    protected SocketConnector newConnector( InetAddress address, int port ) {
+        return new DefaultSocketConnector( address, port, 0, 0 );
+    }
+
+    protected SocketFactory getSocketFactory() {
+        return SocketFactory.getDefault();
+    }
+
+    private ObjectWriter createObjectWriterForSocket() throws IOException {
+        socket.setSoTimeout( acceptConnectionTimeout );
+        ObjectWriter objectWriter = new DirectAutoFlushingObjectWriter( socket.getOutputStream() );
+        socket.setSoTimeout( 0 );
+        return objectWriter;
+    }
+
+    public String getRemoteHost() {
+        return remoteHost;
+    }
+
+    public void setRemoteHost( String host ) {
+        remoteHost = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort( int port ) {
+        this.port = port;
+    }
+
+    public int getQueueSize() {
+        return queueSize;
+    }
+
+    public void setQueueSize( int queueSize ) {
+        this.queueSize = queueSize;
+    }
+
+    public Duration getEventDelayLimit() {
+        return eventDelayLimit;
+    }
+
+    public void setEventDelayLimit( Duration eventDelayLimit ) {
+        this.eventDelayLimit = eventDelayLimit;
+    }
+
+    void setAcceptConnectionTimeout( int acceptConnectionTimeout ) {
+        this.acceptConnectionTimeout = acceptConnectionTimeout;
+    }
+
+    private static class DirectAutoFlushingObjectWriter implements ObjectWriter {
+        private final OutputStream outputStream;
+
+        public DirectAutoFlushingObjectWriter( OutputStream outputStream ) {
+            this.outputStream = outputStream;
+        }
+
+        @Override
+        public void write( Object object ) throws IOException {
+            throw new NotImplementedException( "" );
+        }
+
+        public void write( Externalizable externalizable ) throws IOException {
+            externalizable.writeExternal( new ObjectOutputStream( outputStream ) );
+            outputStream.flush();
+        }
     }
 }
