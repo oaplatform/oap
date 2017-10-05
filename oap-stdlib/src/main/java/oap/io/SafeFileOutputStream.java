b675084ff073079cebd65c334421a1de75d9f32b
igor.petrenko <nofate@ukr.net>
2017-10-05 10:45:44 +0300

    update

--- oap-stdlib/src/main/java/oap/io/SafeFileOutputStream.java
+++ oap-stdlib/src/main/java/oap/io/SafeFileOutputStream.java
@@ -52,7 +52,7 @@ public class SafeFileOutputStream extends FileOutputStream {
     public void close() throws IOException {
         super.close();
         final Path unsafePath = Paths.get( this.path + ".unsafe" );
-        if( !java.nio.file.Files.exists( unsafePath ) )
+        if( !java.nio.file.Files.exists( unsafePath ) || java.nio.file.Files.size( unsafePath ) == 0 )
             Files.delete( unsafePath );
         else
             Files.rename( unsafePath, this.path );
