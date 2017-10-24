c74119da53a11463378957b027e831845e5735fb
igor.petrenko <nofate@ukr.net>
2017-10-24 09:10:54 +0300

    add Stream::grouped

--- oap-stdlib/src/main/java/oap/util/Stream.java
+++ oap-stdlib/src/main/java/oap/util/Stream.java
@@ -23,6 +23,7 @@
  */
 package oap.util;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Enumeration;
@@ -131,6 +132,10 @@ public class Stream<E> implements java.util.stream.Stream<E> {
                 return false;
             }
         };
+    }
+
+    public Stream<List<E>> grouped( int batchSize ) {
+        return of( StreamSupport.stream( new BatchSpliterator<>( underlying.spliterator(), batchSize ), underlying.isParallel() ) );
     }
 
     public <B, C> Stream<C> zip( java.util.stream.Stream<? extends B> b,
@@ -503,5 +508,47 @@ public class Stream<E> implements java.util.stream.Stream<E> {
     public void drain() {
         underlying.forEach( consume() );
         close();
+    }
+
+    private static class BatchSpliterator<E> implements Spliterator<List<E>> {
+        private final Spliterator<E> base;
+        private final int batchSize;
+
+        public BatchSpliterator( Spliterator<E> base, int batchSize ) {
+            this.base = base;
+            this.batchSize = batchSize;
+        }
+
+        @Override
+        public boolean tryAdvance( Consumer<? super List<E>> action ) {
+            final List<E> batch = new ArrayList<>( batchSize );
+            for( int i = 0; i < batchSize && base.tryAdvance( batch::add ); i++ )
+                ;
+            if( batch.isEmpty() )
+                return false;
+            action.accept( batch );
+            return true;
+        }
+
+        @Override
+        public Spliterator<List<E>> trySplit() {
+            if( base.estimateSize() <= batchSize )
+                return null;
+            final Spliterator<E> splitBase = this.base.trySplit();
+            return splitBase == null ? null
+                : new BatchSpliterator<>( splitBase, batchSize );
+        }
+
+        @Override
+        public long estimateSize() {
+            final double baseSize = base.estimateSize();
+            return baseSize == 0 ? 0
+                : ( long ) Math.ceil( baseSize / ( double ) batchSize );
+        }
+
+        @Override
+        public int characteristics() {
+            return base.characteristics();
+        }
     }
 }
