91544143affc161aa2f1d6eeeb200beffb66312a
igor.petrenko <nofate@ukr.net>
2017-12-27 11:11:58 +0200

    fix: logout

--- oap-security/oap-security-ws/src/main/java/oap/security/ws/Logout2WS.java
+++ oap-security/oap-security-ws/src/main/java/oap/security/ws/Logout2WS.java
@@ -29,14 +29,12 @@ import oap.ws.WsMethod;
 import oap.ws.WsParam;
 import oap.ws.security.User;
 import oap.ws.validate.ValidationErrors;
-import oap.ws.validate.WsValidate;
 
 import java.util.Objects;
 
 import static java.lang.String.format;
 import static java.net.HttpURLConnection.HTTP_FORBIDDEN;
 import static oap.http.Request.HttpMethod.GET;
-import static oap.ws.WsParam.From.QUERY;
 import static oap.ws.WsParam.From.SESSION;
 
 /**
@@ -51,11 +49,10 @@ public class Logout2WS {
     }
 
     @WsMethod( method = GET, path = "/" )
-    @WsValidate( { "validateUserAccess" } )
-    public void logout( @WsParam( from = QUERY ) String email, @WsParam( from = SESSION ) User user ) {
-        log.debug( "Invalidating token for user [{}]", email );
+    public void logout( @WsParam( from = SESSION ) String userid ) {
+        log.debug( "Invalidating token for user [{}]", userid );
 
-        authService.invalidateUser( email );
+        authService.invalidateUser( userid );
     }
 
     @SuppressWarnings( "unused" )
