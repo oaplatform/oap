80485ceeea0fc4cec72cfb18606275b93d7d1582
igor.petrenko <nofate@ukr.net>
2017-12-27 09:53:19 +0200

    add: login/logout/auth

---
+++ oap-security/oap-security-ws/src/main/java/oap/security/ws/Auth2WS.java
@@ -0,0 +1,49 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) Open Application Platform Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package oap.security.ws;
+
+import oap.ws.WsMethod;
+import oap.ws.WsParam;
+
+import java.util.Optional;
+
+import static oap.http.Request.HttpMethod.GET;
+import static oap.ws.WsParam.From.PATH;
+
+/**
+ * Created by igor.petrenko on 27.12.2017.
+ */
+public class Auth2WS {
+    private final AuthService2 authService;
+
+    public Auth2WS( AuthService2 authService ) {
+        this.authService = authService;
+    }
+
+    @WsMethod( method = GET, path = "/{tokenId}" )
+    public Optional<Token2> getToken( @WsParam( from = PATH ) String tokenId ) {
+        return authService.getToken( tokenId );
+    }
+}
