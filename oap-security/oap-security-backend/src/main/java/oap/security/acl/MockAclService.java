ecc5a33ae2d3b5e4fcea852bc4a3707a2217feed
igor.petrenko <nofate@ukr.net>
2018-03-22 09:35:46 +0200

    add: mock AclService

---
+++ oap-security/oap-security-backend/src/main/java/oap/security/acl/MockAclService.java
@@ -0,0 +1,128 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) Open Application Platform Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package oap.security.acl;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import oap.util.IdBean;
+import oap.util.Pair;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
+import static oap.util.Pair.__;
+
+/**
+ * Created by igor.petrenko on 21.12.2017.
+ */
+@Slf4j
+public class MockAclService implements AclService {
+    private final HashMap<Pair<String, String>, List<String>> checks = new HashMap<>();
+
+    public void addCheck( String objectId, String subjectId, List<String> permissions ) {
+        checks.put( __( objectId, subjectId ), permissions );
+    }
+
+    @Override
+    public List<SubjectRole> getSubjectRoles( String objectId, boolean inherited ) {
+        return emptyList();
+    }
+
+    @Override
+    public List<ObjectRole> getRoles( String userId, boolean inherited ) {
+        return emptyList();
+    }
+
+    @Override
+    public void validate( String objectId, String subjectId, String... permissions ) throws AclSecurityException {
+        val p = checkAll( objectId, subjectId );
+
+        if( !p.containsAll( asList( permissions ) ) ) {
+            throw new AclSecurityException();
+        }
+    }
+
+    @Override
+    public List<Boolean> check( String objectId, String subjectId, List<String> permissions ) {
+        val p = checkAll( objectId, subjectId );
+
+        return permissions.stream().map( p::contains ).collect( toList() );
+    }
+
+    @Override
+    public List<String> checkAll( String objectId, String subjectId ) {
+        return checks.getOrDefault( __( objectId, subjectId ), emptyList() );
+    }
+
+    @Override
+    public boolean add( String objectId, String subjectId, String roleId, boolean inherit ) {
+        return false;
+    }
+
+    @Override
+    public boolean remove( String objectId, String subjectId, Optional<String> roleId ) {
+        return false;
+    }
+
+    @Override
+    public List<AclRole> list( String objectId, String subjectId ) {
+        return emptyList();
+    }
+
+    @Override
+    public List<String> getChildren( String parentId, String type, boolean recursive ) {
+        return emptyList();
+    }
+
+    @Override
+    public List<String> getChildren( String parentId, String type, boolean recursive, String subjectId, String permission ) {
+        return emptyList();
+    }
+
+    @Override
+    public Predicate<SecurityContainer<? extends IdBean>> getAclFilter( String parentId, String subjectId, String permission ) {
+        return ( p ) -> false;
+    }
+
+    @Override
+    public <T extends IdBean> Optional<SecurityContainer<T>> addChild( String parentId, T object, String type, String owner ) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<AclObject> addChild( String parentId, String id ) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void unregisterObject( String objectId ) {
+
+    }
+}
