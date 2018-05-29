afba24b2f8eebcabc1d5bdce57b9e834cb821781
igor.petrenko <nofate@ukr.net>
2018-05-29 10:09:43 +0300

    http: lazy Response

--- oap-http/src/main/java/oap/http/Client.java
+++ oap-http/src/main/java/oap/http/Client.java
@@ -87,6 +87,7 @@ import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
@@ -272,6 +273,22 @@ public class Client implements Closeable {
     public Response post( String uri, InputStream content, ContentType contentType ) {
         HttpPost request = new HttpPost( uri );
         request.setEntity( new InputStreamEntity( content, contentType ) );
+        return execute( request, Maps.empty(), FOREVER )
+            .orElseThrow( () -> new RuntimeException( "no response" ) );
+    }
+
+    public Response post( String uri, Consumer<OutputStream> content, ContentType contentType ) {
+        HttpPost request = new HttpPost( uri );
+        return post( content, contentType, request );
+    }
+
+    public Response post( URI uri, Consumer<OutputStream> content, ContentType contentType ) {
+        HttpPost request = new HttpPost( uri );
+        return post( content, contentType, request );
+    }
+
+    private Response post( Consumer<OutputStream> content, ContentType contentType, HttpPost request ) {
+        request.setEntity( new HttpOutputStreamEntity( content, contentType ) );
         return execute( request, Maps.empty(), FOREVER )
             .orElseThrow( () -> new RuntimeException( "no response" ) );
     }
