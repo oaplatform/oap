ef6936686809e01f280182c99efb3af85192191d
igor.petrenko <igor.petrenko@xenoss.io>
2019-02-05 18:27:05 +0200

    testng: update listener

--- oap-stdlib/src/main/java/oap/testng/TestListener.java
+++ oap-stdlib/src/main/java/oap/testng/TestListener.java
@@ -8,46 +8,60 @@ package oap.testng;
 
 import com.google.common.base.Throwables;
 import lombok.val;
+import oap.util.Stream;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import java.util.stream.Collectors;
+
 public class TestListener implements ITestListener {
     @Override
     public void onTestStart( ITestResult iTestResult ) {
-        val method = iTestResult.getMethod().getRealClass().getSimpleName() + "." + iTestResult.getMethod().getMethodName();
-        System.out.println( "##teamcity[testStarted name='" + Teamcity.escape( method ) + "' captureStandardOutput='true']" );
+        String method = getMethodName( iTestResult );
+        System.out.println( "#1#teamcity[testStarted name='" + Teamcity.escape( method ) + "' captureStandardOutput='true']" );
+    }
+
+    private String getParameters( ITestResult iTestResult ) {
+        val parameters = iTestResult.getParameters();
+        if( parameters.length == 0 ) return "";
+
+        return Stream.of( parameters ).map( Object::toString ).collect( Collectors.joining( ",", "[", "]" ) );
     }
 
     @Override
     public void onTestSuccess( ITestResult iTestResult ) {
-        val method = iTestResult.getMethod().getRealClass().getSimpleName() + "." + iTestResult.getMethod().getMethodName();
+        String method = getMethodName( iTestResult );
         val time = iTestResult.getEndMillis() - iTestResult.getStartMillis();
-        System.out.println( "##teamcity[testFinished name='" + Teamcity.escape( method ) + "' duration='" + time + "']" );
+        System.out.println( "#1#teamcity[testFinished name='" + Teamcity.escape( method ) + "' duration='" + time + "']" );
+    }
+
+    public String getMethodName( ITestResult iTestResult ) {
+        return iTestResult.getMethod().getRealClass().getSimpleName() + "." + iTestResult.getMethod().getMethodName() + getParameters( iTestResult );
     }
 
     @Override
     public void onTestFailure( ITestResult iTestResult ) {
-        val method = iTestResult.getMethod().getRealClass().getSimpleName() + "." + iTestResult.getMethod().getMethodName();
+        String method = getMethodName( iTestResult );
         val t = iTestResult.getThrowable();
         val message = t != null ? t.getMessage() : "";
         val details = t != null ? Throwables.getStackTraceAsString( t ) : "";
-        System.out.println( "##teamcity[testFailed type='comparisonFailure' name='" + Teamcity.escape( method ) + "' message='" + Teamcity.escape( message ) + "' details='" + Teamcity.escape( details ) + "']" );
+        System.out.println( "#1#teamcity[testFailed type='comparisonFailure' name='" + Teamcity.escape( method + getParameters( iTestResult ) ) + "' message='" + Teamcity.escape( message ) + "' details='" + Teamcity.escape( details ) + "']" );
         val time = iTestResult.getEndMillis() - iTestResult.getStartMillis();
-        System.out.println( "##teamcity[testFinished name='" + Teamcity.escape( method ) + "' duration='" + time + "']" );
+        System.out.println( "#1#teamcity[testFinished name='" + Teamcity.escape( method ) + "' duration='" + time + "']" );
     }
 
     @Override
     public void onTestSkipped( ITestResult iTestResult ) {
-        val method = iTestResult.getMethod().getRealClass().getSimpleName() + "." + iTestResult.getMethod().getMethodName();
-        System.out.println( "##teamcity[testIgnored name='" + Teamcity.escape( method ) + "' message='skipped']" );
+        String method = getMethodName( iTestResult );
+        System.out.println( "#1#teamcity[testIgnored name='" + Teamcity.escape( method ) + "' message='skipped']" );
     }
 
     @Override
     public void onTestFailedButWithinSuccessPercentage( ITestResult iTestResult ) {
-        val method = iTestResult.getMethod().getRealClass().getSimpleName() + "." + iTestResult.getMethod().getMethodName();
+        String method = getMethodName( iTestResult );
         val time = iTestResult.getEndMillis() - iTestResult.getStartMillis();
-        System.out.println( "##teamcity[testFinished name='" + Teamcity.escape( method ) + "' duration='" + time + "']" );
+        System.out.println( "#1#teamcity[testFinished name='" + Teamcity.escape( method ) + "' duration='" + time + "']" );
     }
 
     @Override
