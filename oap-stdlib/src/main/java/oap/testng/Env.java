12f9444ff8ee659b8c99e5cc7dc2654f3fe15fad
igor.petrenko <igor.petrenko@xenoss.io>
2019-02-23 16:43:01 +0200

    add: Env::putEnv

--- oap-stdlib/src/main/java/oap/testng/Env.java
+++ oap-stdlib/src/main/java/oap/testng/Env.java
@@ -28,10 +28,12 @@ import lombok.extern.java.Log;
 import lombok.val;
 import oap.io.Files;
 import oap.io.Resources;
+import oap.util.Maps;
 import oap.util.Throwables;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.lang.reflect.Field;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.URI;
@@ -39,6 +41,8 @@ import java.net.URL;
 import java.net.UnknownHostException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Log
@@ -130,5 +134,35 @@ public class Env {
     public static void resetPorts() {
         System.out.println( "ENV::ports = []" );
         ports.clear();
+    }
+
+    @SneakyThrows
+    public static void putEnv( String name, String value ) {
+        val newenv = Maps.of2( name, value );
+
+        try {
+            Class<?> processEnvironmentClass = Class.forName( "java.lang.ProcessEnvironment" );
+            Field theEnvironmentField = processEnvironmentClass.getDeclaredField( "theEnvironment" );
+            theEnvironmentField.setAccessible( true );
+            Map<String, String> env = ( Map<String, String> ) theEnvironmentField.get( null );
+            env.putAll( newenv );
+            Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField( "theCaseInsensitiveEnvironment" );
+            theCaseInsensitiveEnvironmentField.setAccessible( true );
+            Map<String, String> cienv = ( Map<String, String> ) theCaseInsensitiveEnvironmentField.get( null );
+            cienv.putAll( newenv );
+        } catch( NoSuchFieldException e ) {
+            Class[] classes = Collections.class.getDeclaredClasses();
+            Map<String, String> env = System.getenv();
+            for( Class cl : classes ) {
+                if( "java.util.Collections$UnmodifiableMap".equals( cl.getName() ) ) {
+                    Field field = cl.getDeclaredField( "m" );
+                    field.setAccessible( true );
+                    Object obj = field.get( env );
+                    Map<String, String> map = ( Map<String, String> ) obj;
+                    map.clear();
+                    map.putAll( newenv );
+                }
+            }
+        }
     }
 }
