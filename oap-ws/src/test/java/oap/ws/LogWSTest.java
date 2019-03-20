b5c754a9520f27bce3863684195cbdeecfbd34a6
igor.petrenko <igor.petrenko@xenoss.io>
2019-03-20 08:39:45 +0200

    add: LogWS

---
+++ oap-ws/src/test/java/oap/ws/LogWSTest.java
@@ -0,0 +1,50 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) Open Application Platform Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package oap.ws;
+
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.Test;
+
+/**
+ * Created by igor.petrenko on 20.03.2019.
+ */
+@Test( enabled = false )
+@Slf4j
+public class LogWSTest {
+    @Test( enabled = false )
+    public void testLogs() {
+        var logWS = new LogWS();
+
+        log.trace( "test trace" );
+
+        logWS.setLevel( "ERROR", getClass().getPackageName() );
+        log.trace( "test trace1" );
+        log.debug( "test debug1" );
+        log.error( "test error1" );
+
+        logWS.reset();
+        log.trace( "test trace2" );
+    }
+}
