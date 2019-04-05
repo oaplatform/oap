064725030b014c558984dbf9fe2b82236f5e9989
igor.petrenko <igor.petrenko@xenoss.io>
2019-04-05 09:37:57 +0300

    add: Optional benchmark

---
+++ oap-stdlib/src/test/java/oap/perf/OptionalPerformance.java
@@ -0,0 +1,91 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) Open Application Platform Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package oap.perf;
+
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+import static oap.benchmark.Benchmark.benchmark;
+
+/**
+ * Created by igor.petrenko on 05.04.2019.
+ */
+public class OptionalPerformance {
+    public static final int SAMPLES = 10000000;
+    public static final int EXPERIMENTS = 5;
+    private TestOuter outer = new TestOuter( 77 );
+
+    @Test
+    public void testOptionalVsNull() {
+        benchmark( "Optional", SAMPLES, () -> {
+            optionalTest();
+        } ).experiments( EXPERIMENTS ).run();
+
+        benchmark( "Null", SAMPLES, () -> {
+            nullTest();
+        } ).experiments( EXPERIMENTS ).run();
+    }
+
+    public Integer optionalTest() {
+        return Optional.of( outer )
+            .flatMap( o -> Optional.ofNullable( o.nested ) )
+            .flatMap( n -> Optional.ofNullable( n.inner ) )
+            .flatMap( i -> Optional.ofNullable( i.value ) )
+            .get();
+    }
+
+    public Integer nullTest() {
+        return ( outer != null && outer.nested != null && outer.nested.inner != null ) ? outer.nested.inner.value
+            : null;
+    }
+
+    public class TestOuter {
+
+        public TestNested nested;
+
+        public TestOuter( Integer value ) {
+            this.nested = new TestNested( value );
+        }
+    }
+
+    public class TestNested {
+
+        public TestInner inner;
+
+        public TestNested( Integer value ) {
+            this.inner = new TestInner( value );
+        }
+    }
+
+    public class TestInner {
+
+        public Integer value;
+
+        public TestInner( Integer foo ) {
+            this.value = foo;
+        }
+    }
+}
