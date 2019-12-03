4320c557ae11f30702ce3a5d18b70889eb020615
roman.timrov <roman.timrov@xenoss.io>
2019-12-03 12:58:38 +0200

    added basic fs managers for local/s3

---
+++ oap-fs/src/main/java/oap/fs/S3FileManager.java
@@ -0,0 +1,83 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) Open Application Platform Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package oap.fs;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.transfer.Download;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.s3.transfer.Upload;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Map;
+
+@Slf4j
+public class S3FileManager extends AbstractFileManager  {
+
+    private AmazonS3 s3client;
+    private TransferManager transferManager;
+
+    public S3FileManager( Map<String, Path> buckets ) {
+        super( buckets );
+        log.info( "Init s3-file-manager" );
+        var s3Client = AmazonS3ClientBuilder.standard().withCredentials( new ProfileCredentialsProvider() ).build();
+        if( this.s3client == null ) this.s3client = s3Client;
+        transferManager = TransferManagerBuilder.standard().withS3Client( this.s3client ).build();
+    }
+
+    public S3FileManager( Map<String, Path> buckets, AmazonS3 s3client ) {
+        super( buckets );
+        this.s3client = s3client;
+        this.transferManager = TransferManagerBuilder.standard().withS3Client( s3client ).build();
+    }
+
+    public Upload uploadStream( String bucket, S3Data data ) {
+        var metadata = new ObjectMetadata();
+        metadata.setContentLength( data.contentLength );
+        var path = buckets.get( bucket ).resolve( data.name ).toString();
+        return transferManager.upload( bucket, path, data.io, metadata );
+    }
+
+    public Upload uploadFile( String bucket, S3Data data ) {
+        var metadata = new ObjectMetadata();
+        metadata.setContentLength( data.contentLength );
+        var path = buckets.get( bucket ).resolve( data.name ).toString();
+        return transferManager.upload( bucket, path, data.io, metadata );
+    }
+
+    public Download download( String bucket, String relativePath, File destination ) {
+        return transferManager.download( bucket, relativePath, destination );
+    }
+
+
+    public void copyFromTo( String src, String dist ) {
+
+    }
+}
---
+++ oap-fs/src/test/java/oap/fs/S3FileManagerTest.java
@@ -0,0 +1,87 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) Open Application Platform Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package oap.fs;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import io.findify.s3mock.S3Mock;
+import oap.io.Files;
+import oap.testng.Env;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
+import java.nio.file.Path;
+import java.util.Base64;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class S3FileManagerTest {
+    private final static Path TEST_FOLDER = Path.of( "test/testDir" );
+    private final static String TEST_BUCKET = "default";
+
+    private S3Mock api = new S3Mock.Builder().withPort( 9090 ).withInMemoryBackend().build();
+    private S3FileManager fileManager;
+    private Path tmp = Env.tmpPath( "/tmp/test" );
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        api.start();
+        var endpoint = new AwsClientBuilder.EndpointConfiguration( "http://localhost:9090", "us-west-2" );
+        var client = AmazonS3ClientBuilder
+            .standard()
+            .withPathStyleAccessEnabled( true )
+            .withEndpointConfiguration( endpoint )
+            .withCredentials( new AWSStaticCredentialsProvider( new AnonymousAWSCredentials() ) )
+            .build();
+
+        fileManager = new S3FileManager( Map.of( TEST_BUCKET, TEST_FOLDER ), client );
+        client.createBucket( TEST_BUCKET );
+    }
+
+    @AfterClass
+    public void tearDown() throws Exception {
+        Files.cleanDirectory( tmp );
+        api.stop();
+    }
+
+
+    @Test
+    public void readWrite() throws Exception {
+        var inputBytes = Base64.getDecoder().decode( "dGVzdA==" );
+        var data = new S3Data( "file.txt", new ByteArrayInputStream( inputBytes ), (long) inputBytes.length );
+        var resultFile = tmp.resolve( "output.txt" ).toFile();
+
+        var upload = fileManager.uploadStream( TEST_BUCKET, data ).waitForUploadResult();
+        fileManager.download( TEST_BUCKET, upload.getKey(), resultFile ).waitForCompletion();
+
+        assertThat( resultFile.exists() ).isTrue();
+        assertThat( Files.readString( resultFile.toPath() ) ).isEqualTo( "test" );
+    }
+}
