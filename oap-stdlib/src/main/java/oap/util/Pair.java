6bc49e57d6af6cee7e25d871a719cbffe607b16f
Vova Kyrychenko <vladimir.kirichenko@gmail.com>
2020-04-05 17:36:24 +0300

    comparable pairs

--- oap-stdlib/src/main/java/oap/util/Pair.java
+++ oap-stdlib/src/main/java/oap/util/Pair.java
@@ -24,6 +24,7 @@
 
 package oap.util;
 
+import javax.annotation.Nonnull;
 import java.io.Serializable;
 import java.util.Objects;
 import java.util.function.BiFunction;
@@ -52,12 +53,32 @@ public class Pair<K, V> implements Serializable {
         if( this == o ) return true;
         if( o == null || getClass() != o.getClass() ) return false;
 
-        Pair pair = ( Pair ) o;
+        Pair<?, ?> pair = ( Pair<?, ?> ) o;
 
         if( _1 != null ? !Objects.deepEquals( _1, pair._1 ) : pair._1 != null ) return false;
         if( _2 != null ? !Objects.deepEquals( _2, pair._2 ) : pair._2 != null ) return false;
 
         return true;
+    }
+
+    @SuppressWarnings( "CheckStyle" )
+    public K _1() {
+        return _1;
+    }
+
+    @SuppressWarnings( "CheckStyle" )
+    public K _2() {
+        return _1;
+    }
+
+    @SuppressWarnings( "unchecked" )
+    public <CK extends Comparable<CK>, CV extends Comparable<CV>> ComparablePair<CK, CV> toComparable() {
+        return new ComparablePair<>( ( CK ) _1, ( CV ) _2 );
+    }
+
+    @SuppressWarnings( "unchecked" )
+    public <CK extends Comparable<CK>> KeyComparablePair<CK, V> toComparableByKey() {
+        return new KeyComparablePair<>( ( CK ) _1, _2 );
     }
 
     @Override
@@ -78,5 +99,40 @@ public class Pair<K, V> implements Serializable {
     @Override
     public String toString() {
         return "__(" + deepToString( _1 ) + ", " + deepToString( _2 ) + ')';
+    }
+
+    public static class ComparablePair<CK extends Comparable<CK>, CV extends Comparable<CV>> extends Pair<CK, CV> implements Comparable<ComparablePair<CK, CV>> {
+        @SuppressWarnings( "CheckStyle" )
+        public ComparablePair( CK _1, CV _2 ) {
+            super( _1, _2 );
+        }
+
+        @SuppressWarnings( "CheckStyle" )
+        public static <CK extends Comparable<CK>, CV extends Comparable<CV>> ComparablePair<CK, CV> ___( CK k, CV v ) {
+            return new ComparablePair<>( k, v );
+        }
+
+        @Override
+        public int compareTo( @Nonnull ComparablePair<CK, CV> other ) {
+            int result = this._1.compareTo( other._1 );
+            return result == 0 ? this._2.compareTo( other._2 ) : result;
+        }
+    }
+
+    public static class KeyComparablePair<K extends Comparable<K>, V> extends Pair<K, V> implements Comparable<KeyComparablePair<K, V>> {
+        @SuppressWarnings( "CheckStyle" )
+        public KeyComparablePair( K _1, V _2 ) {
+            super( _1, _2 );
+        }
+
+        @SuppressWarnings( "CheckStyle" )
+        public static <K extends Comparable<K>, V extends Comparable<V>> KeyComparablePair<K, V> ___( K k, V v ) {
+            return new KeyComparablePair<>( k, v );
+        }
+
+        @Override
+        public int compareTo( KeyComparablePair<K, V> other ) {
+            return this._1.compareTo( other._1 );
+        }
     }
 }
