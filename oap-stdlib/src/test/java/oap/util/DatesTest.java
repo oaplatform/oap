137e2c512e656bd169c40fabc1987a380b079b81
igor.petrenko <igor.petrenko@xenoss.io>
2022-06-14 16:03:15 +0300

    Dates#PARSER_MULTIPLE_DATETIME: + Dates#FORMAT_DATE

--- oap-stdlib/src/test/java/oap/util/DatesTest.java
+++ oap-stdlib/src/test/java/oap/util/DatesTest.java
@@ -84,8 +84,9 @@ public class DatesTest extends Fixtures {
 
     @Test
     public void testMultipleFormats() {
-        assertThat( Dates.PARSER_MULTIPLE_DATETIME.parseDateTime( "2022-03-11T15:16:12" ) ).isEqualTo( new DateTime( 2022, 3, 11, 15, 16, 12, 0 ) );
-        assertThat( Dates.PARSER_MULTIPLE_DATETIME.parseDateTime( "2022-03-11 15:16:12" ) ).isEqualTo( new DateTime( 2022, 3, 11, 15, 16, 12, 0 ) );
-        assertThat( Dates.PARSER_MULTIPLE_DATETIME.parseDateTime( "2022-03-11T15:16:12.234" ) ).isEqualTo( new DateTime( 2022, 3, 11, 15, 16, 12, 234 ) );
+        assertThat( Dates.PARSER_MULTIPLE_DATETIME.parseDateTime( "2022-03-11T15:16:12" ) ).isEqualTo( new DateTime( 2022, 3, 11, 15, 16, 12, 0, UTC ) );
+        assertThat( Dates.PARSER_MULTIPLE_DATETIME.parseDateTime( "2022-03-11 15:16:12" ) ).isEqualTo( new DateTime( 2022, 3, 11, 15, 16, 12, 0, UTC ) );
+        assertThat( Dates.PARSER_MULTIPLE_DATETIME.parseDateTime( "2022-03-11T15:16:12.234" ) ).isEqualTo( new DateTime( 2022, 3, 11, 15, 16, 12, 234, UTC ) );
+        assertThat( Dates.PARSER_MULTIPLE_DATETIME.parseDateTime( "2022-03-11" ) ).isEqualTo( new DateTime( 2022, 3, 11, 0, 0, 0, 0, UTC ) );
     }
 }
