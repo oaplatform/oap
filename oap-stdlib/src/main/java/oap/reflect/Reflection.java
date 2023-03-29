3d04b30c811b145b0bee0579b19cc96a02eaea7b
Vitalii Diravka <vitalii@apache.org>
2023-03-29 16:51:35 +0300

    Fix Checktyle. [oap-kernel: Fix for service that implements collection interface]

--- oap-stdlib/src/main/java/oap/reflect/Reflection.java
+++ oap-stdlib/src/main/java/oap/reflect/Reflection.java
@@ -42,7 +42,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
