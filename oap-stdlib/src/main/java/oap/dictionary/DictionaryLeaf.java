089cf150befe10670e307736a7cd4257a6923416
alebastrov <alebastrov@mailinator.com>
2023-06-08 18:59:05 +0300

    reverted toString

--- oap-stdlib/src/main/java/oap/dictionary/DictionaryLeaf.java
+++ oap-stdlib/src/main/java/oap/dictionary/DictionaryLeaf.java
@@ -27,7 +27,6 @@ package oap.dictionary;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
