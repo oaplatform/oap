e92966513bc6edb837814bd23a522ffc1e9c61a0
igor.petrenko <igor.petrenko@xenoss.io>
2023-09-06 21:01:07 +0300

    fix: readFile

--- oap-hadoop/src/main/java/oap/hadoop/S3MockFixture.java
+++ oap-hadoop/src/main/java/oap/hadoop/S3MockFixture.java
@@ -1,18 +1,24 @@
 package oap.hadoop;
 
+import com.amazonaws.SdkClientException;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.util.IOUtils;
 import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.PortBinding;
 import com.github.dockerjava.api.model.Ports;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import oap.io.IoStreams;
 import oap.testng.AbstractEnvFixture;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.utility.DockerImageName;
+
+import java.io.IOException;
 
 @Slf4j
 public class S3MockFixture extends AbstractEnvFixture<S3MockFixture> {
@@ -54,8 +60,8 @@ public class S3MockFixture extends AbstractEnvFixture<S3MockFixture> {
         return this;
     }
 
-    public S3MockFixture withInitialBuckets( String... buckets ) {
-        initialBuckets = String.join( ",", buckets );
+    public S3MockFixture withInitialBuckets( String... bucketNames ) {
+        initialBuckets = String.join( ",", bucketNames );
 
         return this;
     }
@@ -68,16 +74,20 @@ public class S3MockFixture extends AbstractEnvFixture<S3MockFixture> {
         super.after();
     }
 
-    public void createBucket( String bucket ) {
-        s3().createBucket( bucket );
+    public void createBucket( String bucketName ) {
+        s3().createBucket( bucketName );
     }
 
-    public void deleteBucket( String bucket ) {
-        s3().deleteBucket( bucket );
+    public void deleteBucket( String bucketName ) {
+        s3().deleteBucket( bucketName );
     }
 
-    public String readFile( String bucket, String name ) {
-        return s3().getObjectAsString( bucket, name );
+    public String readFile( String bucketName, String key ) {
+        try( S3Object object = s3().getObject( bucketName, key ) ) {
+            return IOUtils.toString( IoStreams.in( object.getObjectContent(), IoStreams.Encoding.from( key ) ) );
+        } catch( IOException e ) {
+            throw new SdkClientException( "Error streaming content from S3 during download", e );
+        }
     }
 
     private AmazonS3 s3() {
