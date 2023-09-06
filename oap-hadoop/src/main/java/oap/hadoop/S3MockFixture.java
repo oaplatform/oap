e64eb0ba1d9a451601f1f14ec479fd1f44dd690d
igor.petrenko <igor.petrenko@xenoss.io>
2023-09-06 15:23:00 +0300

    fix: mock

--- oap-hadoop/src/main/java/oap/hadoop/S3MockFixture.java
+++ oap-hadoop/src/main/java/oap/hadoop/S3MockFixture.java
@@ -1,5 +1,9 @@
 package oap.hadoop;
 
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import oap.testng.AbstractEnvFixture;
 import org.testcontainers.containers.GenericContainer;
@@ -9,19 +13,30 @@ import org.testcontainers.utility.DockerImageName;
 @Slf4j
 public class S3MockFixture extends AbstractEnvFixture<S3MockFixture> {
     private static final String S3MOCK_VERSION = "3.1.0";
+    @Getter
+    private final int port;
     private GenericContainer<?> container;
     private boolean debug = false;
     private String initialBuckets = "";
+
+    public S3MockFixture() {
+        definePort( "S3MOCK_PORT" );
+        port = portFor( "S3MOCK_PORT" );
+    }
 
     @Override
     protected void before() {
         super.before();
 
+        var portBinding = new PortBinding(
+            Ports.Binding.bindPort( port ),
+            new ExposedPort( 9090 ) );
+
         container = new GenericContainer<>( DockerImageName.parse( "adobe/s3mock:" + S3MOCK_VERSION ) );
         container
             .withLogConsumer( new Slf4jLogConsumer( log ) )
             .withEnv( "debug", String.valueOf( debug ) )
-            .withExposedPorts( 9090 );
+            .withCreateContainerCmdModifier( cmd -> cmd.getHostConfig().withPortBindings( portBinding ) );
 
         if( !initialBuckets.isEmpty() ) {
             container.withEnv( "initialBuckets", initialBuckets );
@@ -39,10 +54,6 @@ public class S3MockFixture extends AbstractEnvFixture<S3MockFixture> {
         initialBuckets = String.join( ",", buckets );
 
         return this;
-    }
-
-    public int getPort() {
-        return container.getMappedPort( 9090 );
     }
 
     @Override
