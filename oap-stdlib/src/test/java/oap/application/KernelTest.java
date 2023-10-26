c3eba25703a82232c5f5f825e420a68ccca6567f
igor.petrenko <igor.petrenko@xenoss.io>
2023-10-26 20:37:06 +0300

    update

--- oap-stdlib/src/test/java/oap/application/KernelTest.java
+++ oap-stdlib/src/test/java/oap/application/KernelTest.java
@@ -38,7 +38,6 @@ import oap.util.Lists;
 import oap.util.Maps;
 import org.slf4j.Logger;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.net.URL;
