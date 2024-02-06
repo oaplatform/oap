ff75b029e606b968b3b7a9e4a3d05383e10cacf6
igor.petrenko <nofateg@users.noreply.github.com>
2024-02-06 12:15:35 +0200

    OAP-227 Close long lived connections when they have handled X requests

---
+++ oap-stdlib-test/src/test/java/oap/http/server/nio/handlers/KeepaliveRequestsHandlerTest.java
@@ -0,0 +1,49 @@
+package oap.http.server.nio.handlers;
+
+import oap.http.Client;
+import oap.http.Http;
+import oap.http.server.nio.NioHttpServer;
+import oap.testng.EnvFixture;
+import oap.testng.Fixture;
+import oap.testng.Fixtures;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.LinkedHashSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class KeepaliveRequestsHandlerTest extends Fixtures {
+
+    private final int testHttpPort;
+
+    public KeepaliveRequestsHandlerTest() {
+        var envFixture = fixture( new EnvFixture().withScope( Fixture.Scope.CLASS ) );
+        testHttpPort = envFixture.portFor( "TEST_HTTP_PORT" );
+    }
+
+    @Test
+    public void testCloseConnection() throws IOException {
+        var ids = new LinkedHashSet<Long>();
+        try( NioHttpServer httpServer = new NioHttpServer( new NioHttpServer.DefaultPort( testHttpPort ) ) ) {
+
+            KeepaliveRequestsHandler keepaliveRequestsHandler = new KeepaliveRequestsHandler( 2 );
+            httpServer.handlers.add( keepaliveRequestsHandler );
+
+            httpServer.start();
+
+            httpServer.bind( "/test", exchange -> {
+                long id = exchange.exchange.getConnection().getId();
+                ids.add( id );
+                exchange.responseOk( "ok", Http.ContentType.TEXT_PLAIN );
+            } );
+
+            for( int i = 0; i < 5; i++ ) {
+                assertThat( Client.DEFAULT.get( "http://localhost:" + testHttpPort + "/test" ).contentString() ).isEqualTo( "ok" );
+            }
+
+            assertThat( ids ).hasSize( 3 );
+            assertThat( keepaliveRequestsHandler.requests ).hasSize( 1 );
+        }
+    }
+}
---
+++ oap-stdlib/src/main/java/oap/http/server/nio/handlers/KeepaliveRequestsHandler.java
@@ -0,0 +1,47 @@
+package oap.http.server.nio.handlers;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.server.ServerConnection;
+import oap.http.server.nio.AbstractNioHandler;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class KeepaliveRequestsHandler extends AbstractNioHandler implements ServerConnection.CloseListener {
+    public final int keepaliveRequests;
+    final ConcurrentHashMap<Long, AtomicLong> requests = new ConcurrentHashMap<>();
+
+    public KeepaliveRequestsHandler( int keepaliveRequests ) {
+        this.keepaliveRequests = keepaliveRequests;
+    }
+
+    public KeepaliveRequestsHandler( HttpHandler httpHandler, int keepaliveRequests ) {
+        super( httpHandler );
+        this.keepaliveRequests = keepaliveRequests;
+    }
+
+    @Override
+    public void handleRequest( HttpServerExchange exchange ) throws Exception {
+        ServerConnection connection = exchange.getConnection();
+        long id = connection.getId();
+        AtomicLong count = requests.computeIfAbsent( id, connectionId -> {
+            connection.addCloseListener( this );
+            return new AtomicLong( 0L );
+        } );
+        long requests = count.incrementAndGet();
+
+        try {
+            httpHandler.handleRequest( exchange );
+        } finally {
+            if( requests >= keepaliveRequests ) {
+                connection.close();
+            }
+        }
+    }
+
+    @Override
+    public void closed( ServerConnection connection ) {
+        requests.remove( connection.getId() );
+    }
+}
