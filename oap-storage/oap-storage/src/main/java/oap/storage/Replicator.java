b7b0312bad7918194c3828da8488f474bb9dbe8a
igor.petrenko <igor.petrenko@xenoss.io>
2024-03-04 13:55:03 +0200

    add: RemoteStorage

--- oap-storage/oap-storage/src/main/java/oap/storage/Replicator.java
+++ oap-storage/oap-storage/src/main/java/oap/storage/Replicator.java
@@ -30,6 +30,7 @@ import oap.concurrent.scheduler.Scheduled;
 import oap.concurrent.scheduler.Scheduler;
 import oap.storage.Storage.DataListener.IdObject;
 import oap.util.Cuid;
+import oap.util.Lists;
 import oap.util.Pair;
 
 import java.io.Closeable;
@@ -53,9 +54,9 @@ import static oap.util.Pair.__;
 public class Replicator<I, T> implements Closeable {
     static final AtomicLong stored = new AtomicLong();
     static final AtomicLong deleted = new AtomicLong();
-    private String uniqueName = Cuid.UNIQUE.next();
     private final MemoryStorage<I, T> slave;
     private final ReplicationMaster<I, T> master;
+    private String uniqueName = Cuid.UNIQUE.next();
     private Scheduled scheduled;
     private transient Pair<Long, String> lastModified = __( -1L, "" );
 
@@ -121,7 +122,7 @@ public class Replicator<I, T> implements Closeable {
             .filter( metadata -> metadata.modified == finalLastUpdate )
             .map( metadata -> slave.identifier.get( metadata.object ).toString() )
             .sorted()
-            .collect( toList() );
+            .toList();
 
         for( var id : list ) {
             hasher = hasher.putUnencodedChars( id );
@@ -148,9 +149,13 @@ public class Replicator<I, T> implements Closeable {
             stored.addAndGet( newUpdates.size() );
         }
 
+        log.trace( "[{}] added {} updated {}", uniqueName, Lists.map( added, a -> a.id ), Lists.map( updated, a -> a.id ) );
+
         var ids = master.ids();
         log.trace( "[{}] master ids {}", uniqueName, ids );
-        if( ids.isEmpty() ) lastUpdate = -1;
+        if( ids.isEmpty() ) {
+            lastUpdate = -1;
+        }
 
         List<IdObject<I, T>> deleted = slave.memory.selectLiveIds()
             .filter( id -> !ids.contains( id ) )
