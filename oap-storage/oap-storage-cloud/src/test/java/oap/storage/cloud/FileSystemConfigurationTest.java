eef1e6defde391e71b637c96acf9dde51cc94e8f
igor.petrenko <igor.petrenko@xenoss.io>
2024-03-15 13:13:05 +0200

    S3MockFixture#readFile

--- oap-storage/oap-storage-cloud/src/test/java/oap/storage/cloud/FileSystemConfigurationTest.java
+++ oap-storage/oap-storage-cloud/src/test/java/oap/storage/cloud/FileSystemConfigurationTest.java
@@ -11,15 +11,21 @@ public class FileSystemConfigurationTest {
     @Test
     public void getDefault() {
         FileSystemConfiguration fileSystemConfiguration = new FileSystemConfiguration(
-            Map.of( "fs.s3", Map.of(
-                "test-bucket.jclouds.endpoint", "http://localhost/s3/tb",
-                "jclouds.endpoint", "http://localhost/s3"
-            ) )
+            Map.of( "fs.s3.test-bucket.jclouds.endpoint", "http://localhost/s3/tb",
+                "fs.s3.jclouds.endpoint", "http://localhost/s3",
+                "fs", Map.of(
+                    "default.jclouds.scheme", "s3",
+                    "default.jclouds.container", "test-bucket"
+                )
+            )
         );
 
         assertThat( fileSystemConfiguration.get( "s3", "my-container" ) )
             .contains( entry( "jclouds.endpoint", "http://localhost/s3" ) );
         assertThat( fileSystemConfiguration.get( "s3", "test-bucket" ) )
             .contains( entry( "jclouds.endpoint", "http://localhost/s3/tb" ) );
+
+        assertThat( fileSystemConfiguration.getDefaultScheme() ).isEqualTo( "s3" );
+        assertThat( fileSystemConfiguration.getDefaultContainer() ).isEqualTo( "test-bucket" );
     }
 }
