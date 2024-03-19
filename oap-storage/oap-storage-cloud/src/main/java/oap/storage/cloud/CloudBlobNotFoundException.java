0fa5389a93109d38b92412340e1045b849f5e73e
igor.petrenko <igor.petrenko@xenoss.io>
2024-03-19 15:15:45 +0200

    FileSystem#createContainer

--- oap-storage/oap-storage-cloud/src/main/java/oap/storage/cloud/CloudBlobNotFoundException.java
+++ oap-storage/oap-storage-cloud/src/main/java/oap/storage/cloud/CloudBlobNotFoundException.java
@@ -1,6 +1,11 @@
 package oap.storage.cloud;
 
+import java.io.Serial;
+
 public class CloudBlobNotFoundException extends CloudException {
+    @Serial
+    private static final long serialVersionUID = 1602763875095843228L;
+
     public CloudBlobNotFoundException( CloudURI blob ) {
         super( blob.toString() );
     }
