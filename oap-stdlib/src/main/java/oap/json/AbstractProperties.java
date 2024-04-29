81a20cb0ea5be551db1ed74fd8846fd5ca392496
igor.petrenko <nofateg@users.noreply.github.com>
2024-04-29 18:12:55 +0300

    AbstractProperties

---
+++ oap-stdlib/src/main/java/oap/json/AbstractProperties.java
@@ -0,0 +1,37 @@
+package oap.json;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import oap.json.properties.PropertiesDeserializer;
+
+import javax.annotation.Nullable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ToString
+@EqualsAndHashCode
+public abstract class AbstractProperties {
+    @JsonIgnore
+    private final LinkedHashMap<String, Object> properties = new LinkedHashMap<>();
+
+    @JsonAnySetter
+    @JsonDeserialize( contentUsing = PropertiesDeserializer.class )
+    public void putProperty( String name, Object value ) {
+        properties.put( name, value );
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getProperties() {
+        return properties;
+    }
+
+    @SuppressWarnings( "unchecked" )
+    @Nullable
+    public <T> T getProperty( String property ) {
+        return ( T ) properties.get( property );
+    }
+}
