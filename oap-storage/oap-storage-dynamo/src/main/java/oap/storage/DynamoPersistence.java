fe07f709897192c0bb3de61ddb561ecb86148297
igor.petrenko <igor.petrenko@xenoss.io>
2024-08-01 18:17:50 +0300

    OAP-000 disable ThrottleLoginInterceptor with delay <= 0

--- oap-storage/oap-storage-dynamo/src/main/java/oap/storage/DynamoPersistence.java
+++ oap-storage/oap-storage-dynamo/src/main/java/oap/storage/DynamoPersistence.java
@@ -103,7 +103,7 @@ public class DynamoPersistence<I, T> extends AbstractPersistance<I, T> implement
     }
 
     @Override
-    protected synchronized void processRecords( CountDownLatch cdl ) {
+    protected void processRecords( CountDownLatch cdl ) {
         TableDescription table = dynamodbClient.describeTable( tableName, null );
         String streamArn = table.latestStreamArn();
         cdl.countDown();
@@ -133,7 +133,7 @@ public class DynamoPersistence<I, T> extends AbstractPersistance<I, T> implement
     }
 
     @Override
-    public synchronized void fsync() {
+    public void fsync() {
         var time = DateTimeUtils.currentTimeMillis();
         synchronizedOn( lock, () -> {
             if( stopped ) return;
