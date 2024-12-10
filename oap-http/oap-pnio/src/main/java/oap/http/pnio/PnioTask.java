2a04a6121e374832ed7ab7751b41a6a26ad6de9a
igor.petrenko <igor.petrenko@xenoss.io>
2024-12-10 10:26:28 +0200

    OAP-226 pnio: v2

--- oap-http/oap-pnio/src/main/java/oap/http/pnio/PnioTask.java
+++ oap-http/oap-pnio/src/main/java/oap/http/pnio/PnioTask.java
@@ -1,24 +1,99 @@
 package oap.http.pnio;
 
-import io.undertow.server.HttpServerExchange;
+import lombok.SneakyThrows;
+import org.joda.time.DateTimeUtils;
 
-public class PnioTask extends AbstractPnioTask {
-    private final Runnable task;
+import java.lang.reflect.Field;
+import java.nio.channels.Selector;
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import java.util.TreeSet;
 
-    public PnioTask( Object workerThread, HttpServerExchange exchange, long timeout, Runnable task ) {
-        super( workerThread, exchange, timeout );
-        this.task = task;
+public class PnioTask<WorkflowState> implements Runnable {
+    private static Field selectorWorkQueueField;
+    private static Field workLockField;
+    private static Field selectorField;
+    private static Field delayWorkQueueField;
+
+    static {
+        init();
+    }
+
+    public final PnioExchange<WorkflowState> pnioExchange;
+    protected final ArrayDeque<Runnable> selectorWorkQueue;
+    protected final Object workLock;
+    protected final Selector selector;
+    protected final TreeSet<?> delayWorkQueue;
+
+    @SneakyThrows
+    @SuppressWarnings( "unchecked" )
+    public PnioTask( Object workerThread, PnioExchange<WorkflowState> pnioExchange ) {
+        selectorWorkQueue = ( ArrayDeque<Runnable> ) selectorWorkQueueField.get( workerThread );
+        delayWorkQueue = ( TreeSet<?> ) delayWorkQueueField.get( workerThread );
+        workLock = workLockField.get( workerThread );
+        selector = ( Selector ) selectorField.get( workerThread );
+
+        this.pnioExchange = pnioExchange;
+    }
+
+    @SneakyThrows
+    private static void init() {
+        Class<?> workerThreadClass = Class.forName( "org.xnio.nio.WorkerThread" );
+
+        selectorWorkQueueField = workerThreadClass.getDeclaredField( "selectorWorkQueue" );
+        selectorWorkQueueField.setAccessible( true );
+
+        delayWorkQueueField = workerThreadClass.getDeclaredField( "delayWorkQueue" );
+        delayWorkQueueField.setAccessible( true );
+
+        workLockField = workerThreadClass.getDeclaredField( "workLock" );
+        workLockField.setAccessible( true );
+
+        selectorField = workerThreadClass.getDeclaredField( "selector" );
+        selectorField.setAccessible( true );
+    }
+
+    public boolean isTimeout( long now ) {
+        return ( System.nanoTime() - pnioExchange.getRequestStartTime() ) > pnioExchange.timeoutNano;
     }
 
     @Override
     public void run() {
-        task.run();
+        pnioExchange.process();
 
-        super.run();
+        synchronized( workLock ) {
+            long now = DateTimeUtils.currentTimeMillis();
+
+            Iterator<Runnable> iterator = selectorWorkQueue.descendingIterator();
+            while( iterator.hasNext() ) {
+                Runnable runnable = iterator.next();
+                if( runnable instanceof PnioTask<?> pnioTask ) {
+                    if( pnioTask.pnioExchange.timeoutNano > 0 ) {
+                        if( pnioTask.isTimeout( now ) ) {
+                            iterator.remove();
+
+                            pnioExchange.pnioListener.fireOnTimeout( pnioExchange );
+                        } else {
+                            break;
+                        }
+                    } else if( pnioExchange.maxQueueSize > selectorWorkQueue.size() ) {
+                        iterator.remove();
+
+                        pnioExchange.pnioListener.fireOnRejected( pnioExchange );
+                    }
+                }
+            }
+        }
     }
 
-    public void register() {
+    public void register( boolean init ) {
         synchronized( workLock ) {
+            if( init ) {
+                if( selectorWorkQueue.size() > pnioExchange.maxQueueSize ) {
+                    pnioExchange.pnioListener.fireOnRejected( pnioExchange );
+                    return;
+                }
+            }
             selectorWorkQueue.add( this );
         }
         selector.wakeup();
