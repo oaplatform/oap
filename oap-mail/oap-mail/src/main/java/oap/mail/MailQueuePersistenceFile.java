8c1b160515427af5b7c362baebaecc81b71bc4e5
igor.petrenko <igor.petrenko@xenoss.io>
2025-01-17 15:54:24 +0200

    oap-mail: mongo persistence

--- oap-mail/oap-mail/src/main/java/oap/mail/MailQueuePersistenceFile.java
+++ oap-mail/oap-mail/src/main/java/oap/mail/MailQueuePersistenceFile.java
@@ -1,7 +1,5 @@
 package oap.mail;
 
-import io.micrometer.core.instrument.Metrics;
-import io.micrometer.core.instrument.Tags;
 import lombok.extern.slf4j.Slf4j;
 import oap.json.Binder;
 import oap.reflect.TypeRef;
@@ -10,34 +8,20 @@ import oap.util.Lists;
 import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Slf4j
-public class MailQueuePersistenceFile implements MailQueuePersistence {
-    private final ConcurrentLinkedQueue<Message> queue = new ConcurrentLinkedQueue<>();
+public class MailQueuePersistenceFile extends MailQueuePersistenceMemory {
     private final Path location;
 
     public MailQueuePersistenceFile( Path location ) {
         this.location = location.resolve( "mail.gz" );
         load();
-
-        Metrics.gaugeCollectionSize( "mail_queue", Tags.empty(), queue );
     }
 
     private void load() {
         log.debug( "loading queue..." );
         queue.addAll( Binder.json.unmarshal( new TypeRef<List<Message>>() {}, location ).orElse( Lists.empty() ) );
         log.debug( "{} messages loaded", size() );
-    }
-
-    @Override
-    public int size() {
-        return queue.size();
-    }
-
-    @Override
-    public void add( Message message ) {
-        queue.add( message );
     }
 
     private synchronized void persist() {
