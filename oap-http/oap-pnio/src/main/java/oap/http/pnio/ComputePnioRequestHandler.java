a7d39fc10d8be15e27f5a325d12f41f4837f56c9
igor.petrenko <igor.petrenko@xenoss.io>
2025-01-31 18:52:50 +0200

    bump deps

--- oap-http/oap-pnio/src/main/java/oap/http/pnio/ComputePnioRequestHandler.java
+++
@@ -1,8 +0,0 @@
-package oap.http.pnio;
-
-import java.io.IOException;
-
-@SuppressWarnings( "checkstyle:AbstractClassName" )
-public abstract class ComputePnioRequestHandler<State> extends AbstractPnioRequestHandler<State> {
-    public abstract void handle( PnioExchange<State> pnioExchange, State state ) throws InterruptedException, IOException;
-}
