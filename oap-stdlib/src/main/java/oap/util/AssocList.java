4bb64f4d231fe91f89b353a62253079d8689c1be
igor.petrenko <igor.petrenko@xenoss.io>
2025-03-07 21:05:54 +0200

    AssocList is now threadsafe

--- oap-stdlib/src/main/java/oap/util/AssocList.java
+++ oap-stdlib/src/main/java/oap/util/AssocList.java
@@ -25,16 +25,27 @@ package oap.util;
 
 import com.google.common.base.Preconditions;
 
+import javax.annotation.concurrent.ThreadSafe;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+import java.util.Spliterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
+@ThreadSafe
 @SuppressWarnings( "checkstyle:AbstractClassName" )
-public abstract class AssocList<K, V> extends LinkedHashSet<V> {
+public abstract class AssocList<K, V> implements Collection<V> {
+    private final CopyOnWriteArrayList<V> list = new CopyOnWriteArrayList<>();
+
     public AssocList() {
     }
 
@@ -57,12 +68,37 @@ public abstract class AssocList<K, V> extends LinkedHashSet<V> {
         return this.stream().filter( v -> Objects.equals( key, keyOf( v ) ) ).findAny();
     }
 
+    public V get( int index ) {
+        return list.get( index );
+    }
+
     public V getOrDefault( K key, V def ) {
         return get( key ).orElse( def );
     }
 
     public boolean removeKey( K key ) {
         return removeIf( o -> hasKey( o, key ) );
+    }
+
+    @Override
+    public boolean remove( Object o ) {
+        return list.remove( o );
+    }
+
+    public V remove( int index ) {
+        return list.remove( index );
+    }
+
+    public V set( int index, V element ) {
+        return list.set( index, element );
+    }
+
+    public V getFirst() {
+        return list.getFirst();
+    }
+
+    public V getLast() {
+        return list.getLast();
     }
 
     protected boolean hasKey( V o, K key ) {
@@ -97,14 +133,94 @@ public abstract class AssocList<K, V> extends LinkedHashSet<V> {
     }
 
     @Override
-    public boolean add( V v ) {
+    public synchronized boolean add( V v ) {
         removeKey( keyOf( v ) );
-        return super.add( v );
+        return list.add( v );
     }
 
     @Override
-    public boolean addAll( Collection<? extends V> c ) {
+    public synchronized boolean addAll( Collection<? extends V> c ) {
         for( V v : c ) add( v );
         return !c.isEmpty();
+    }
+
+    @Override
+    public Iterator<V> iterator() {
+        return list.iterator();
+    }
+
+    @Override
+    public void forEach( Consumer<? super V> action ) {
+        list.forEach( action );
+    }
+
+    @Override
+    public Spliterator<V> spliterator() {
+        return list.spliterator();
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public boolean contains( Object o ) {
+        return list.contains( o );
+    }
+
+    @Override
+    public Object[] toArray() {
+        return list.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray( T[] a ) {
+        return list.toArray( a );
+    }
+
+    @Override
+    public <T> T[] toArray( IntFunction<T[]> generator ) {
+        return list.toArray( generator );
+    }
+
+    @Override
+    public boolean containsAll( Collection<?> c ) {
+        return list.containsAll( c );
+    }
+
+    @Override
+    public boolean removeAll( Collection<?> c ) {
+        return list.removeAll( c );
+    }
+
+    @Override
+    public boolean retainAll( Collection<?> c ) {
+        return list.retainAll( c );
+    }
+
+    @Override
+    public void clear() {
+        list.clear();
+    }
+
+    @Override
+    public boolean removeIf( Predicate<? super V> filter ) {
+        return list.removeIf( filter );
+    }
+
+    @Override
+    public Stream<V> stream() {
+        return list.stream();
+    }
+
+    @Override
+    public Stream<V> parallelStream() {
+        return list.parallelStream();
     }
 }
