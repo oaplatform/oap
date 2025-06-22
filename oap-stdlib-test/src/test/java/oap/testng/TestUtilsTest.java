3e2592abe773a262eaf9ccda16ca32beb828827d
igor.petrenko <igor.petrenko@xenoss.io>
2025-06-22 11:55:27 +0300

    add: TestUtils

---
+++ oap-stdlib-test/src/test/java/oap/testng/TestUtilsTest.java
@@ -0,0 +1,24 @@
+package oap.testng;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class TestUtilsTest {
+    @BeforeMethod
+    public void beforeMethod() {
+        String rand = TestUtils.randomName( "{rand}" );
+        assertThat( TestUtils.randomName( "{test_class_name}-{rand}" ) ).isEqualTo( "UtilsTest-" + rand );
+
+        assertThatThrownBy( () -> TestUtils.randomName( "{test_class_name}-{test_method_name}-{rand}" ) )
+            .isInstanceOf( IllegalArgumentException.class );
+    }
+
+    @Test
+    public void testRandomName() {
+        String rand = TestUtils.randomName( "{rand}" );
+        assertThat( TestUtils.randomName( "{test_class_name}-{test_method_name}-{rand}" ) ).isEqualTo( "UtilsTest-testRandomName-" + rand );
+    }
+}
