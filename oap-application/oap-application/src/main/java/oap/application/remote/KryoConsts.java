ed6724a73c809106f0bc03fdfe9311700fb5d3a8
igor.petrenko <igor.petrenko@xenoss.io>
2025-11-27 19:34:11 +0200

    fix: fory -> kryo

---
+++ oap-application/oap-application/src/main/java/oap/application/remote/KryoConsts.java
@@ -0,0 +1,32 @@
+package oap.application.remote;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+import java.io.ByteArrayOutputStream;
+
+@SuppressWarnings( "checkstyle:AbstractClassName" )
+public abstract class KryoConsts {
+    public static Kryo kryo;
+
+    static {
+        kryo = new Kryo();
+        kryo.setRegistrationRequired( false );
+    }
+
+    public static byte[] writeClassAndObject( Object obj ) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try( Output output = new Output( byteArrayOutputStream ) ) {
+            kryo.writeClassAndObject( output, obj );
+        }
+
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    public static Object readClassAndObject( byte[] bytes ) {
+        Input input = new Input( bytes );
+
+        return kryo.readClassAndObject( input );
+    }
+}
