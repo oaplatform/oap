4e75d73fb71a52c8c400e7e6b0557b230d88b86b
igor.petrenko <igor.petrenko@xenoss.io>
2025-11-28 00:50:26 +0200

    kryo pool

--- oap-application/oap-application/src/main/java/oap/application/remote/KryoConsts.java
+++ oap-application/oap-application/src/main/java/oap/application/remote/KryoConsts.java
@@ -7,31 +7,58 @@ import com.esotericsoftware.kryo.util.DefaultInstantiatorStrategy;
 import org.objenesis.strategy.SerializingInstantiatorStrategy;
 
 import java.io.ByteArrayOutputStream;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 @SuppressWarnings( "checkstyle:AbstractClassName" )
 public abstract class KryoConsts {
-    public static Kryo kryo;
+    private static final ConcurrentLinkedDeque<Kryo> queue = new ConcurrentLinkedDeque<>();
 
-    static {
-        kryo = new Kryo();
+    private static Kryo newInstance() {
+        Kryo kryo = new Kryo();
         kryo.setRegistrationRequired( false );
         kryo.setReferences( true );
         kryo.setInstantiatorStrategy( new DefaultInstantiatorStrategy( new SerializingInstantiatorStrategy() ) );
 
+        return kryo;
     }
 
     public static byte[] writeClassAndObject( Object obj ) {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         try( Output output = new Output( byteArrayOutputStream ) ) {
-            kryo.writeClassAndObject( output, obj );
+            writeClassAndObject( output, obj );
         }
 
         return byteArrayOutputStream.toByteArray();
     }
 
+    public static void writeClassAndObject( Output output, Object obj ) {
+        Kryo kryo = queue.poll();
+        if( kryo == null ) {
+            kryo = newInstance();
+        }
+        try {
+            kryo.writeClassAndObject( output, obj );
+        } finally {
+            queue.push( kryo );
+        }
+    }
+
     public static Object readClassAndObject( byte[] bytes ) {
         Input input = new Input( bytes );
 
-        return kryo.readClassAndObject( input );
+        return readClassAndObject( input );
+    }
+
+    public static Object readClassAndObject( Input input ) {
+        Kryo kryo = queue.poll();
+        if( kryo == null ) {
+            kryo = newInstance();
+        }
+        try {
+            return kryo.readClassAndObject( input );
+        } finally {
+            queue.push( kryo );
+        }
+
     }
 }
