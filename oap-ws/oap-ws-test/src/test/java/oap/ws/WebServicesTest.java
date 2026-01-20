975414f259e26dbe0ffc942bb679503603fbb706
igor.petrenko <igor.petrenko@xenoss.io>
2026-01-20 11:54:33 +0200

    CE-132 oap-sso: user: replace PK: email -> id

--- oap-ws/oap-ws-test/src/test/java/oap/ws/WebServicesTest.java
+++ oap-ws/oap-ws-test/src/test/java/oap/ws/WebServicesTest.java
@@ -96,7 +96,7 @@ public class WebServicesTest extends Fixtures {
 
     @Test
     public void cookie() {
-        assertGet( kernel.httpUrl( "/x/v/math/cookie" ), Map.of(), Map.of( "Cookie", "cookie=theCookie;Really-Cool-Cookie=ohoh" ) )
+        assertGet( kernel.httpUrl( "/x/v/math/cookie" ), Map.of(), Map.of( "cookie", "cookie=theCookie;Really-Cool-Cookie=ohoh" ) )
             .responded( OK, "OK", APPLICATION_JSON, "\"theCookieohoh\"" );
     }
 
