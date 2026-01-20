44b19c1cb89747289fcd45bba341d1901f1fd18c
igor.petrenko <igor.petrenko@xenoss.io>
2026-01-20 19:40:46 +0200

    CE-132 oap-sso: user: replace PK: email -> id

--- oap-stdlib-test/src/main/java/oap/json/testng/JsonAsserts.java
+++ oap-stdlib-test/src/main/java/oap/json/testng/JsonAsserts.java
@@ -46,7 +46,6 @@ import static oap.json.Binder.json;
 import static oap.testng.Asserts.assertString;
 import static oap.testng.Asserts.contentOfTestResource;
 import static oap.util.Pair.__;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class JsonAsserts {
     @Deprecated
@@ -190,13 +189,13 @@ public class JsonAsserts {
         }
 
         private JsonAssertion isEqualCanonically( Class<?> clazz, String actual, String expected ) {
-            assertThat( json.canonicalizeWithDefaultPrettyPrinter( clazz, actual ) )
+            assertString( json.canonicalizeWithDefaultPrettyPrinter( clazz, actual ) )
                 .isEqualTo( json.canonicalizeWithDefaultPrettyPrinter( clazz, expected ) );
             return this;
         }
 
         private JsonAssertion isEqualCanonically( TypeRef<?> typeRef, String actual, String expected ) {
-            assertThat( json.canonicalizeWithDefaultPrettyPrinter( typeRef, actual ) )
+            assertString( json.canonicalizeWithDefaultPrettyPrinter( typeRef, actual ) )
                 .isEqualTo( json.canonicalizeWithDefaultPrettyPrinter( typeRef, expected ) );
             return this;
         }
