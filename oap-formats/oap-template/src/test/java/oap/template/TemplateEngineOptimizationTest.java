6e3cc24529ac0ecf87cf83ff43aa21c0ef378420
igor.petrenko <igor.petrenko@xenoss.io>
2026-04-28 18:38:24 +0300

    CE-153 oap-template: optimize RowBinaryObjectLogger

--- oap-formats/oap-template/src/test/java/oap/template/TemplateEngineOptimizationTest.java
+++ oap-formats/oap-template/src/test/java/oap/template/TemplateEngineOptimizationTest.java
@@ -19,9 +19,29 @@ public class TemplateEngineOptimizationTest extends AbstractTemplateEngineTest {
 
         assertThat( listener.javaCode )
             .containsOnlyOnce( """
-                oap.template.TestTemplateClass s_child = s.child;
-                // --- with ( child ) START BODY
-                if ( s_child != null ) {
-            """ );
+                    oap.template.TestTemplateClass s_child = s.child;
+                    // --- with ( child ) START BODY
+                    if ( s_child != null ) {
+                """ )
+            .containsOnlyOnce( """
+                    } else {
+                      acc.acceptNull( java.lang.String.class );
+                      acc.acceptText( "-" );
+                      acc.acceptNull( java.lang.String.class );
+                    }
+                """ );
+    }
+
+    @Test
+    public void testBlockWithMultipleFieldsAndComments() {
+        TestTemplateClass c = new TestTemplateClass();
+        c.child = new TestTemplateClass();
+        c.child.field = "f1";
+        c.child.field2 = "f2";
+        assertThat( getTemplate( testMethodName, new TypeRef<TestTemplateClass>() {},
+            "{{% with child }}{{ /** c field */ field }}-{{ /** c field2 */field2 }}{{% end }}", STRING, null ).render( c ).get() )
+            .isEqualTo( "f1-f2" );
+
+        assertThat( listener.javaCode ).containsOnlyOnce( "if ( s_child != null ) {" );
     }
 }
